8e48d3ea2abb2ec5d761c372e9c4f8abd97d9822
prologic <none@none>
2008-11-06 21:25:55 +1000

    lib: Added doc strign

---
+++ circuits/lib/__init__.py
@@ -0,0 +1,8 @@
+# Module:	__init__
+# Date:		3rd October 2008
+# Author:	James Mills, prologic at shortcircuit dot net dot au
+
+"""Circuits Library
+
+circuits.lib contains a standard suite of Components for circuits.
+"""
