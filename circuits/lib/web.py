cb23aa1061bf096cd0df216e5672874688aacd3d
prologic <none@none>
2008-11-07 12:40:27 +1000

    lib.web: Added basic WSGI Application support

--- circuits/lib/web.py
+++ circuits/lib/web.py
@@ -11,20 +11,22 @@ server implementation with support for headers, cookies, positional
 and keyword arguments, filtering, url dispatching and more.
 """
 
+import sys
 from inspect import getargspec
 
-from circuits import Component
+from circuits import listener, Component
 from circuits.lib.sockets import TCPServer
 from circuits.lib.http import HTTP, Dispatcher
+from circuits.lib.http import Request, Response, _Request, _Response, Headers
 
 def expose(*args, **kwargs):
 	def decorate(f):
-		def wrapper(self, request, listener, *args_, **kwargs_):
+		def wrapper(self, request, response, *args_, **kwargs_):
 			self.request = request
-			self.listener = listener
+			self.response = response
 			ret = f(self, *args_, **kwargs_)
 			del self.request
-			del self.listener
+			del self.response
 			return ret
  
 		wrapper.type = kwargs.get("type", "listener")
@@ -53,6 +55,62 @@ class Server(TCPServer):
 				self.poll()
 			except KeyboardInterrupt:
 				break
+
+class Application(Component):
+
+	headerNames = {
+			"HTTP_CGI_AUTHORIZATION": "Authorization",
+		   "CONTENT_LENGTH": "Content-Length",
+		   "CONTENT_TYPE": "Content-Type",
+		   "REMOTE_HOST": "Remote-Host",
+		   "REMOTE_ADDR": "Remote-Addr",}
+
+	def __init__(self, *args, **kwargs):
+		super(Application, self).__init__(*args, **kwargs)
+
+		self += Dispatcher()
+
+	def translateHeaders(self, environ):
+		for cgiName in environ:
+			# We assume all incoming header keys are uppercase already.
+			if cgiName in self.headerNames:
+				yield self.headerNames[cgiName], environ[cgiName]
+			elif cgiName[:5] == "HTTP_":
+				# Hackish attempt at recovering original header names.
+				translatedHeader = cgiName[5:].replace("_", "-")
+				yield translatedHeader, environ[cgiName]
+	
+	def getRequestResponse(self, environ):
+		env = environ.get
+		
+		headers = Headers(list(self.translateHeaders(environ)))
+
+		request = _Request(
+				env("REQUEST_METHOD"),
+				env("PATH_INFO"),
+				env("SERVER_PROTOCOL"),
+				env("QUERY_STRING"),
+				headers)
+
+		request.script_name = env("SCRIPT_NAME")
+		request.wsgi_environ = environ
+
+		request.body = env("wsgi.input")
+		response = _Response(None)
+
+		response.gzip = "gzip" in request.headers.get("Accept-Encoding", "")
+
+		return request, response
+
+	@listener("response")
+	def onRESPONSE(self, response):
+		response.start_response(response.status, response.headers.items())
+
+	def __call__(self, environ, start_response):
+		request, response = self.getRequestResponse(environ)
+		response.start_response = start_response
+		self.send(Request(request, response), "request")
+		return [response.body]
 
 class _AutoListener(type):
 
