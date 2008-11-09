b434f02380a2d3d8fc2f35eb78870ca2e79676b7
prologic <none@none>
2008-11-09 15:40:11 +1000

    examples/eventserver.py: Added simple event server example demonstrating the Bridge Component and basic request/reponse

---
+++ examples/eventserver.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim: set sw=3 sts=3 ts=3
+
+"""(Example) Event Server
+
+An example of using circuits and the event bdirge to create a simple 
+server/client system. This is a really simple example and only to
+demonstrate the Bridge Component.
+
+This example demonstrates:
+	* Basic Request/Response model using events.
+	* Briding systems/components.
+
+This example makes use of:
+	* Component
+	* Bridge
+	* Manager
+"""
+
+import circuits
+from circuits import Debugger
+from circuits import listener, Event, Component, Bridge, Manager
+from circuits.lib.io import Stdin
+
+###
+### Components
+###
+
+class Server(Component):
+
+	@listener("hello")
+	def onHELLO(self, message):
+		self.push(Event("Received: %s" % message), "received")
+
+###
+### Main
+###
+
+def main():
+	manager = Manager()
+	debugger = Debugger()
+	bridge = Bridge(8000)
+
+	debugger.IgnoreEvents = ["Read", "Write"]
+
+	manager += bridge
+	manager += debugger
+	manager += Server()
+
+	while True:
+		try:
+			manager.flush()
+			bridge.poll()
+		except KeyboardInterrupt:
+			break
+
+###
+### Entry Point
+###
+
+if __name__ == "__main__":
+	main()
