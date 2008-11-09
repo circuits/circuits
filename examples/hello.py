8848e0559cd6cc05fbc74c80042b192724c3be44
prologic <none@none>
2008-11-09 20:40:26 +1000

    examples/hello.py: Greatly simplified and documented

--- examples/hello.py
+++ examples/hello.py
@@ -5,54 +5,36 @@
 """(Example) Hello World
 
 A trivial simple example of using circuits to print a simple
-message.
-
-This example demonstrates:
-	* Basic Component creation.
-	* Basic Event handling.
-
-This example makes use of:
-	* Component
-	* Event
-	* Manager
+message. This example demonstrates the basics of circutis'
+Components and Events and basic Event handling.
 """
 
 from circuits.core import listener, Event, Component, Manager
 
-###
-### Events
-###
-
 class Message(Event):
-	"""Message(Event) -> Message Event
+	"""Message Event
 
 	args: message
 	"""
 
-###
-### Components
-###
-
 class Printer(Component):
+	"""Printer Component
+
+	A Component that will listen to "print" Events and print each message
+	received.
+	"""
 
 	@listener("print")
 	def onPRINT(self, message=""):
+		"""Print Event Handler
+
+		When a "print" Event has been received, print the message.
+		"""
+
 		print message
 
-###
-### Main
-###
-
-def main():
+if __name__ == "__main__":
 	manager = Manager()
 	manager += Printer()
-
 	manager.push(Message("Hello World"), "print")
 	manager.flush()
-
-###
-### Entry Point
-###
-
-if __name__ == "__main__":
-	main()
