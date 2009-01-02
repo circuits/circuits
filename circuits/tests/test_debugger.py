535ee485322b8a2c61dde443edc49e37419235a1
jmills <none@none>
2009-01-02 16:18:06 +1000

    tests/test_debugger: Fixed Debugger tests

--- circuits/tests/test_debugger.py
+++ circuits/tests/test_debugger.py
@@ -1,6 +1,6 @@
-# Module:	test_debugger
-# Date:		5th November 2008
-# Author:	James Mills, prologic at shortcircuit dot net dot au
+# Module:   test_debugger
+# Date:     5th November 2008
+# Author:   James Mills, prologic at shortcircuit dot net dot au
 
 """Debugger Test Suite
 
@@ -16,205 +16,162 @@ from circuits import listener, Event, Component, Manager
 
 
 class Test(Event):
-	"Test(Event) -> Test Event"
+    "Test(Event) -> Test Event"
 
 class Foo(Component):
 
-	@listener("foo")
-	def onFOO(self):
-		pass
+    @listener("foo")
+    def onFOO(self):
+        pass
 
-	@listener("bar")
-	def onBAR(self):
-		pass
+    @listener("bar")
+    def onBAR(self):
+        pass
 
-class Logger(Component):
+class Logger(object):
 
-	event = None
-	channel = "log"
+    msg = None
 
-	@listener("debug")
-	def onDEBUG(self, event, *args, **kwargs):
-		self.event = args[0]
+    def debug(self, msg):
+        self.msg = msg
 
 
 class EventTestCase(unittest.TestCase):
 
-	def setUp(self):
-		sys.stderr = StringIO()
+    def setUp(self):
+        sys.stderr = StringIO()
 
-	def testDebugger(self):
-		"""Test Debugger
+    def testDebugger(self):
+        """Test Debugger
 
-		Test Debugger
-		"""
+        Test Debugger
+        """
 
-		manager = Manager()
-		debugger = Debugger()
-		foo = Foo()
+        manager = Manager()
+        debugger = Debugger()
+        foo = Foo()
 
-		manager += debugger
-		manager += foo
+        manager += debugger
+        manager += foo
 
-		debugger.enable()
-		e = Event()
-		manager.send(e, "foo")
-		sys.stderr.seek(0)
-		s = sys.stderr.read().strip()
-		self.assertEquals(s, str(e))
+        debugger.enabled = True
+        e = Event()
+        manager.send(e, "foo")
+        sys.stderr.seek(0)
+        s = sys.stderr.read().strip()
+        self.assertEquals(s, str(e))
 
-		sys.stderr.seek(0)
-		sys.stderr.truncate()
+        sys.stderr.seek(0)
+        sys.stderr.truncate()
 
-		debugger.disable()
-		e = Event()
-		manager.send(e, "foo")
-		sys.stderr.seek(0)
-		s = sys.stderr.read().strip()
-		self.assertEquals(s, "")
+        debugger.enabled = False
+        e = Event()
+        manager.send(e, "foo")
+        sys.stderr.seek(0)
+        s = sys.stderr.read().strip()
+        self.assertEquals(s, "")
 
-		sys.stderr.seek(0)
-		sys.stderr.truncate()
+        sys.stderr.seek(0)
+        sys.stderr.truncate()
 
-		debugger.toggle()
-		e = Event()
-		manager.send(e, "foo")
-		sys.stderr.seek(0)
-		s = sys.stderr.read().strip()
-		self.assertEquals(s, str(e))
 
-		sys.stderr.seek(0)
-		sys.stderr.truncate()
+    def testIgnoreEvents(self):
+        """Test Debugger's IgnoreEvents
 
-		debugger.toggle()
-		e = Event()
-		manager.send(e, "foo")
-		sys.stderr.seek(0)
-		s = sys.stderr.read().strip()
-		self.assertEquals(s, "")
+        Test Debugger's IgnoreEvents
+        """
 
-		sys.stderr.seek(0)
-		sys.stderr.truncate()
+        manager = Manager()
+        debugger = Debugger()
+        foo = Foo()
 
-		debugger.set(True)
-		e = Event()
-		manager.send(e, "foo")
-		sys.stderr.seek(0)
-		s = sys.stderr.read().strip()
-		self.assertEquals(s, str(e))
+        manager += debugger
+        manager += foo
 
-		sys.stderr.seek(0)
-		sys.stderr.truncate()
+        debugger.IgnoreEvents.extend(["Test"])
+        debugger.enabled = True
 
-		debugger.set(False)
-		e = Event()
-		manager.send(e, "foo")
-		sys.stderr.seek(0)
-		s = sys.stderr.read().strip()
-		self.assertEquals(s, "")
+        e = Event()
+        manager.send(e, "foo")
+        sys.stderr.seek(0)
+        s = sys.stderr.read().strip()
+        self.assertEquals(s, str(e))
 
-		sys.stderr.seek(0)
-		sys.stderr.truncate()
-		foo.unregister()
+        sys.stderr.seek(0)
+        sys.stderr.truncate()
 
-	def testIgnoreEvents(self):
-		"""Test Debugger's IgnoreEvents
+        e = Test()
+        manager.send(e, "foo")
+        sys.stderr.seek(0)
+        s = sys.stderr.read().strip()
+        self.assertEquals(s, "")
 
-		Test Debugger's IgnoreEvents
-		"""
+        sys.stderr.seek(0)
+        sys.stderr.truncate()
+        foo.unregister()
 
-		manager = Manager()
-		debugger = Debugger()
-		foo = Foo()
+    def testIgnoreChannels(self):
+        """Test Debugger's IgnoreChannels
 
-		manager += debugger
-		manager += foo
+        Test Debugger's IgnoreChannels
+        """
 
-		debugger.IgnoreEvents.extend(["Test"])
-		debugger.enable()
+        manager = Manager()
+        debugger = Debugger()
+        foo = Foo()
 
-		e = Event()
-		manager.send(e, "foo")
-		sys.stderr.seek(0)
-		s = sys.stderr.read().strip()
-		self.assertEquals(s, str(e))
+        manager += debugger
+        manager += foo
 
-		sys.stderr.seek(0)
-		sys.stderr.truncate()
+        debugger.IgnoreChannels.extend(["bar"])
+        debugger.enabled = True
 
-		e = Test()
-		manager.send(e, "foo")
-		sys.stderr.seek(0)
-		s = sys.stderr.read().strip()
-		self.assertEquals(s, "")
+        e = Event()
+        manager.send(e, "foo")
+        sys.stderr.seek(0)
+        s = sys.stderr.read().strip()
+        self.assertEquals(s, str(e))
 
-		sys.stderr.seek(0)
-		sys.stderr.truncate()
-		foo.unregister()
+        sys.stderr.seek(0)
+        sys.stderr.truncate()
 
-	def testIgnoreChannels(self):
-		"""Test Debugger's IgnoreChannels
+        e = Event()
+        manager.send(e, "bar")
+        sys.stderr.seek(0)
+        s = sys.stderr.read().strip()
+        self.assertEquals(s, "")
 
-		Test Debugger's IgnoreChannels
-		"""
+        sys.stderr.seek(0)
+        sys.stderr.truncate()
+        foo.unregister()
 
-		manager = Manager()
-		debugger = Debugger()
-		foo = Foo()
+    def testLogger(self):
+        """Test Debugger Logger
 
-		manager += debugger
-		manager += foo
+        Test Debugger using dummy Logger-like
+        object (normally logging.Logger)
+        """
 
-		debugger.IgnoreChannels.extend(["bar"])
-		debugger.enable()
+        manager = Manager()
+        logger = Logger()
+        foo = Foo()
 
-		e = Event()
-		manager.send(e, "foo")
-		sys.stderr.seek(0)
-		s = sys.stderr.read().strip()
-		self.assertEquals(s, str(e))
+        debugger = Debugger(logger=logger)
+        manager += debugger
+        manager += foo
 
-		sys.stderr.seek(0)
-		sys.stderr.truncate()
+        e = Event()
+        manager.push(e, "foo")
+        manager.flush()
+        manager.flush()
+        self.assertEquals(logger.msg, repr(e))
 
-		e = Event()
-		manager.send(e, "bar")
-		sys.stderr.seek(0)
-		s = sys.stderr.read().strip()
-		self.assertEquals(s, "")
-
-		sys.stderr.seek(0)
-		sys.stderr.truncate()
-		foo.unregister()
-
-	def testLogger(self):
-		"""Test Debugger Logger
-
-		Test Debugger using dummy Logger-like
-		object (normally logging.Logger)
-		"""
-
-		manager = Manager()
-		logger = Logger()
-		foo = Foo()
-
-		debugger = Debugger(logger=True)
-		manager += logger
-		manager += debugger
-		manager += foo
-
-		e = Event()
-		manager.push(e, "foo")
-		manager.flush()
-		manager.flush()
-		self.assertEquals(logger.event, str(e))
-
-		foo.unregister()
+        foo.unregister()
 
 
 def suite():
-	return unittest.makeSuite(EventTestCase, "test")
+    return unittest.makeSuite(EventTestCase, "test")
 
 
 if __name__ == "__main__":
-	unittest.main()
+    unittest.main()
