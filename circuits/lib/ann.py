d9c47ffebe22e9890cbf7ee2827bfcc4b4bf78eb
jmills <none@none>
2009-01-09 16:51:49 +1000

    lib.ann: Compatibility fixes

--- circuits/lib/ann.py
+++ circuits/lib/ann.py
@@ -10,84 +10,47 @@
 import math
 from time import time, sleep
 
-from circuits import listener, Event, Thread
+from circuits.workers import Thread
+from circuits import listener, Event, Component
 
-
-class SignalEvent(Event): pass
-
+class Signal(Event): pass
 
 class Node(Thread):
 
    def __init__(self, *args, **kwargs):
       super(Node, self).__init__(*args, **kwargs)
 
-      self._inputs = []
+      self.start()
 
    def __repr__(self):
-      return "<Node running=%s>" % self.isRunning()
-
-   def _getInputs(self):
-      return self._inputs
-
-   def link(self, node):
-      Worker.link(self, node)
-      node._inputs.append(self)
+      return "<Node running=%s>" % self.running
 
    def fire(self, level=1.0):
-      self.event.push(SignalEvent(self, level), "signal")
+      self.push(Signal(level), "signal")
 
    def run(self):
-      while self.isRunning():
+      while self.running:
          self.flush()
          sleep(0.01)
-
-   inputs = property(_getInputs)
-
-
-def new_node(*args, **kwargs):
-
-   class NewNode(Node):
-      pass
-   return NewNode(*args, **kwargs)
-
 
 class Connection(Node):
 
    def __init__(self, *args, **kwargs):
       super(Connection, self).__init__(*args, **kwargs)
 
-      self._weight = kwargs.get("weight", 1.0)
+      self.weight = kwargs.get("weight", 1.0)
 
    def __repr__(self):
-      return "<Connection weight=%0.2f>" % self._weight
-
-   def _get_weight(self):
-      try:
-         return self._weight
-      except:
-         return None
-
-   def _set_weight(self, weight):
-      self._weight = weight
+      return "<Connection weight=%0.2f>" % self.weight
 
    @listener("signal")
-   def onSIGNAL(self, source, level):
-      self.fire(level * self._weight)
-
-   weight = property(_get_weight, _set_weight)
-
-
-def new_connection(*args, **kwargs):
-
-   class NewConnection(Connection):
-      pass
-   return NewConnection(*args, **kwargs)
-
+   def onSIGNAL(self, level):
+      self.fire(level * self.weight)
 
 class _StepNeuron(object):
 
    def compute(self):
-      if self._level >= self._threshold:
+      if self.level >= self.threshold:
          self.fire(1.0)
       else:
          self.fire(0.0)
@@ -96,7 +59,7 @@ class _StepNeuron(object):
 class _LinearNeuron(object):
 
    def compute(self):
-      self.fire(self._level + self._threshold)
+      self.fire(self.level + self.threshold)
 
 
 class _SigmoidNeuron(object):
@@ -105,7 +68,7 @@ class _SigmoidNeuron(object):
       self.fire(
             1.0 / (
                1.0 + math.exp(-1 * (
-                  self._level + self._threshold)
+                  self.level + self.threshold)
                   )
                )
             )
@@ -114,20 +77,22 @@ class _SigmoidNeuron(object):
 class Neuron(Node):
 
    def __init__(self, *args, **kwargs):
+      self.ls = None
       super(Neuron, self).__init__(*args, **kwargs)
 
-      self._threshold = threshold
-      self._level = 0.0
-      self._ls = None
+      self.threshold = kwargs.get("threshold", 1.0)
+      self.level = 0.0
+
+      type = kwargs.get("type", "step")
 
       if type == "step":
-         self._type = "Step"
+         self.type = "Step"
          base = _StepNeuron
       elif type == "linear":
-         self._type = "Linear"
+         self.type = "Linear"
          base = _LinearNeuron
       elif type == "sigmoid":
-         self._type = "Sigmoid"
+         self.type = "Sigmoid"
          base = _SigmoidNeuron
       else:
          raise TypeError("Invalid type")
@@ -136,58 +101,32 @@ class Neuron(Node):
 
    def __repr__(self):
       return "<Neuron type=%s threshold=%0.2f level=%0.2f>" % (
-            self._type, self._threshold, self._level)
-
-   def _get_threshold(self):
-      try:
-         return self._threshold
-      except:
-         return None
-
-   def _set_threshold(self, threshold):
-      self._threshold = threshold
+            self.type, self.threshold, self.level)
 
    @listener("signal")
-   def onSIGNAL(self, source, level):
-      self._ls = time()
-      self._level += level
+   def onSIGNAL(self, level):
+      self.ls = time()
+      self.level += level
 
    def run(self):
-      while self.isRunning():
-         if self._ls is not None and (time() - self._ls > 0.01):
+      while self.running:
+         if self.ls is not None and (time() - self.ls > 0.01):
             self.compute()
-            self._level = 0.0
-            self._ls = None
-         self.event.flush()
+            self.level = 0.0
+            self.ls = None
+         self.flush()
          sleep(0.01)
-
-   threshold = property(_get_threshold, _set_threshold)
-
-def new_neuron(*args, **kwargs):
-   class NewNeuron(Neuron):
-      pass
-   return NewNeuron(*args, **kwargs)
-
 
 class Output(Node):
 
    def __init__(self, *args, **kwargs):
       super(Output, self).__init__(*args, **kwargs)
 
-      self._f = f
-      self._output = None
+      self.output = 0.0
 
-   def _getOutput(self):
-      return self._output
+   def __repr__(self):
+      return "<Output output=%0.2f>" % self.output
 
    @listener("signal")
-   def onSIGNAL(self, source, level):
-      self._output = level
-      self._f(self)
-
-   output = property(_getOutput)
-
-def new_output(*args, **kwargs):
-   class NewOutput(Output):
-      pass
-   return NewOutput(*args, **kwargs)
+   def onSIGNAL(self, level):
+      self.output = level
