331c635c49301787213620ffb536f2ce3cb9c3d9
jmills <none@none>
2009-01-16 13:54:12 +1000

    workers: Fixed Process to share running state properly so the process can be stopped properly

--- circuits/workers.py
+++ circuits/workers.py
@@ -1,27 +1,18 @@
 #!/usr/bin/env python
 
-import os
-
 from threading import Thread as _Thread
 from threading import enumerate as threads
 
 from multiprocessing import Pipe as _Pipe
+from multiprocessing import Value as _Value
 from multiprocessing import Process as _Process
 
 from multiprocessing import cpu_count as cpus
 from multiprocessing import active_children as processes
 
-from circuits.core import listener, Event
 from circuits.core import Component as _Component
 
 POLL_INTERVAL = 0.00001
-BUFFER_SIZE = 131072
-
-class Read(Event): pass
-class Helo(Event): pass
-class Error(Event): pass
-class Write(Event): pass
-class Close(Event): pass
 
 class Thread(_Component):
 
@@ -41,42 +32,64 @@ class Thread(_Component):
     def stop(self):
         self.running = False
 
+    def isAlive(self):
+        return self.running
+
 class Process(_Component):
 
     def __init__(self, *args, **kwargs):
         super(Process, self).__init__(*args, **kwargs)
 
-        self.running = False
-        self.process = _Process(target=self._run, name=self.__class__.__name__)
+        self.running = _Value("b", False)
+        self.thread = _Thread(target=self.run)
+        self.process = _Process(target=self._run, args=(self.running,))
         self.parent, self.child = _Pipe()
 
-    def _run(self):
-        thread = _Thread(target=self.run)
-        thread.start()
+    def _run(self, running):
+        self.thread.start()
 
-        while self.running:
-            try:
-                self.flush()
-                if self.child.poll(POLL_INTERVAL):
-                    event = self.child.recv()
-                    channel = event.channel
-                    target = event.target
-                    self.send(event, channel, target)
-            except KeyboardInterrupt:
-                self.running = False
-                break
-
-        thread.join()
+        try:
+            while running.value:
+                try:
+                    self.flush()
+                    if self.child.poll(POLL_INTERVAL):
+                        event = self.child.recv()
+                        channel = event.channel
+                        target = event.target
+                        self.send(event, channel, target)
+                except SystemExit:
+                    running.acquire()
+                    running.value = False
+                    running.release()
+                    break
+                except KeyboardInterrupt:
+                    running.acquire()
+                    running.value = False
+                    running.release()
+                    break
+        finally:
+            running.acquire()
+            running.value = False
+            running.release()
+            self.thread.join()
+            self.flush()
 
     def start(self):
-        self.running = True
+        self.running.acquire()
+        self.running.value = True
+        self.running.release()
         self.process.start()
 
     def run(self):
         pass
 
     def stop(self):
-        self.running = False
+        self.running.acquire()
+        self.running.value = False
+        self.running.release()
+
+    def isAlive(self):
+        return self.running.value
 
     def poll(self, wait=POLL_INTERVAL):
         if self.parent.poll(POLL_INTERVAL):
