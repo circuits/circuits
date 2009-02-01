7a6061c6f7e7c74aac85e3b53efd68a479476ef7
prologic <none@none>
2009-02-01 21:55:40 +1000

    drivers: New drivers module importing from lib.drivers.*

---
+++ circuits/drivers.py
@@ -0,0 +1,10 @@
+# Module:	drivers
+# Date:		1st February 2009
+# Author:	James Mills, prologic at shortcircuit dot net dot au
+
+"""Circuits Library - Drivers
+
+circuits.lib.drivers contains drivers for other sources of events.
+"""
+
+from circuits.lib.drivers import *
