1a64ac028660a9ee75e27720c0e64d50f3a24cae
prologic <none@none>
2009-02-01 21:59:02 +1000

    web: New web module importing from lib.web.*

---
+++ circuits/web.py
@@ -0,0 +1,10 @@
+# Module:	web
+# Date:		1st February 2009
+# Author:	James Mills, prologic at shortcircuit dot net dot au
+
+"""Circuits Library - Web
+
+circuits.lib.web contains the circuits full stack web server and wsgi library.
+"""
+
+from circuits.lib.web import *
