1936b40ec931224b88d58a50a7630e969786ea84
prologic <none@none>
2009-02-16 02:36:31 +1000

    lib.web: Import Logger

--- circuits/lib/web/__init__.py
+++ circuits/lib/web/__init__.py
@@ -7,6 +7,7 @@
 circuits.lib.web contains the circuits full stack web server and wsgi library.
 """
 
+from loggers import Logger
 from core import Controller
 from errors import Redirect
 from events import Request, Response
