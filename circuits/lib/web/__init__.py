b35db1bc5f21ac598e47155a5029b59339de22f4
prologic <none@none>
2009-02-16 02:47:52 +1000

    lib.web: import FileServer (from dispatchers)

--- circuits/lib/web/__init__.py
+++ circuits/lib/web/__init__.py
@@ -10,6 +10,7 @@ circuits.lib.web contains the circuits full stack web server and wsgi library.
 from loggers import Logger
 from core import Controller
 from errors import Redirect
+from dispatchers import FileServer
 from events import Request, Response
 from servers import BaseServer, Server
 from wsgi import Application, Middleware, Filter
