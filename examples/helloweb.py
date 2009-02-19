11c95051da304bb96b19b4f3caf58d695edbbd1c
prologic <none@none>
2009-02-20 04:56:14 +1000

    examples/helloweb.py: Simplified and added psyco

--- examples/helloweb.py
+++ examples/helloweb.py
@@ -9,30 +9,19 @@ to the user's browser. This example demonstrates how to build a
 simple but complete web server and application using circuits.
 """
 
+import psyco; psyco.full()
+
 from circuits import Component
-from circuits.web import BaseServer, Response
+from circuits.web import BaseServer
 
-class HelloWorld(Component):
-    """HelloWorld Component
+class Root(Component):
 
-    A Component that will listen to "index" Events and return an appropiate
-    HTTP Response with the message "Hello Worl!".
-    """
-
-    def request(self, request, response, *args, **kwargs):
-        """Index Event Handler
-
-        HTTP Request Handler that listens on the "index' channel which maps
-        to http://localhost:8000/ When a Request is received, send back an
-        appropiate HTTP Response with contens of the body being "Hello World!"
-        """
-
-        response.body = "Hello World!"
-        self.send(Response(response), "response")
+    def request(self, request, response):
+        return "Hello World!"
 
 def main():
     server = BaseServer(8000)
-    server += HelloWorld()
+    server += Root()
     server.run()
 
 if __name__ == "__main__":
