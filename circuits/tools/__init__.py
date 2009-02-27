e75185bbb0824d02b69bab40ef31cee7f886cfe1
prologic <none@none>
2009-02-27 12:28:26 +1000

    tools: Added graph function to return a string representing the structure of registered components

--- circuits/tools/__init__.py
+++ circuits/tools/__init__.py
@@ -7,3 +7,46 @@
 circuits.tools contains a standard set of tools for circuits. These
 tools are installed as executables with a prefix of "circuits."
 """
+
+try:
+    from cStringIO import StringIO
+except ImportError:
+    from StringIO import StringIO
+
+def graph(x):
+    s = StringIO()
+
+    d = 0
+    i = 0
+    done = False
+    stack = []
+    visited = set()
+    children = list(x.components)
+    while not done:
+        if x not in visited:
+            if d:
+                s.write("%s%s\n" % (" " * d, "|"))
+                s.write("%s%s%s\n" % (" " * d, "|-", x))
+            else:
+                s.write(" .%s\n" % x)
+
+            if x.components:
+                d += 1
+
+            visited.add(x)
+
+        if i < len(children):
+            x = children[i]
+            i += 1
+            if x.components:
+                stack.append((i, children))
+                children = list(x.components)
+                i = 0
+        else:
+            if stack:
+                i, children = stack.pop()
+                d -= 1
+            else:
+                done = True
+
+    return s.getvalue()
