674665b47d3f6f4115e664f031350c7f25109058
prologic <none@none>
2009-03-02 09:25:14 +1000

    examples/filewatch.py: New example to watch the contents of a file. Basically a tail clone

---
+++ examples/filewatch.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import sys
+from time import sleep
+from circuits import Component
+
+class FileWatcher(Component):
+
+    def __init__(self, filename):
+        self.filename = filename
+        self.fp = open(self.filename, "r")
+
+    def __tick__(self):
+        self.fp.seek(self.fp.tell())
+        for line in self.fp.readlines():
+            sys.stdout.write(line) 
+        sleep(0.1)
+
+filewatcher = FileWatcher(sys.argv[1])
+filewatcher.run()
