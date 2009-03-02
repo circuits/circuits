0d8e825aebb584639e521225f4d0cc42924cabdb
prologic <none@none>
2009-03-03 02:28:18 +1000

    examples/pygameex.py: Simplified

--- examples/pygameex.py
+++ examples/pygameex.py
@@ -6,14 +6,15 @@ import pygame
 from pygame import K_q
 from pygame import DOUBLEBUF, HWSURFACE
 
+from circuits import Component, Debugger
 from circuits.drivers import PyGameDriver
-from circuits import Component, Debugger, Manager
 
 class Test(Component):
 
     def __init__(self, *args, **kwargs):
         super(Test, self).__init__(*args, **kwargs)
-
+        pygame.init()
+        self += PyGameDriver() + Debugger()
         screen = pygame.display.set_mode((640, 480), DOUBLEBUF | HWSURFACE)
 
     def keyup(self, key, mod):
@@ -23,25 +24,5 @@ class Test(Component):
     def quit(self):
         raise SystemExit, 0
 
-def main():
-
-    manager = Manager()
-    manager += Debugger()
-
-    driver = PyGameDriver()
-    test = Test()
-
-    manager += driver
-    manager += test
-
-    while True:
-        try:
-            driver.poll()
-            manager.flush()
-        except KeyboardInterrupt:
-            break
-        except SystemExit:
-            break
-
 if __name__ == "__main__":
-    main()
+    Test().run()
