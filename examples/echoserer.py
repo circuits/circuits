050ce3cb079c0a565aceb64c21228bff09e2f576
prologic <none@none>
2009-03-03 13:40:13 +1000

    examples/echoserver.py: Fixed and simplified

--- examples/echoserer.py
+++ examples/echoserer.py
@@ -1,52 +1,12 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# vim: set sw=3 sts=3 ts=3
-
-"""(Example) Echo Server
-
-A simple Echo Server example that sends back to connected clients
-the input the server receieves.
-
-This example demonstrates:
-    * Basic Component creation.
-    * Basic Event handling.
-    * Basic TCP Server
-
-This example makes use of:
-    * Component
-    * Event
-    * Manager
-    * lib.sockets.TCPServer
-"""
 
 from circuits.lib.sockets import TCPServer
-
-###
-### Components
-###
 
 class EchoServer(TCPServer):
 
     def read(self, sock, data):
-        self.write(sock, data)
+        print "%s: %s" % (sock, data.strip())
+        self.write(sock, "Got: %s" % data.strip())
     
-###
-### Main
-###
-
-def main():
-    server = EchoServer(8000)
-
-    while True:
-        try:
-            server.flush()
-            server.poll()
-        except KeyboardInterrupt:
-            break
-
-###
-### Entry Point
-###
-
 if __name__ == "__main__":
-    main()
+    EchoServer(8000).run()
