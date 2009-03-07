863b959ea6877d2bcb0102bdf447c30202e9f0f1
prologic <none@none>
2009-03-08 07:05:26 +1000

    lib.pollers: Completed implementation of Select, Poll and EPoll pollers

--- circuits/lib/pollers.py
+++ circuits/lib/pollers.py
@@ -12,80 +12,72 @@ descriptors for read/write events. Pollers:
    - KQueue
 """
 
-try:
-    from select26.select import select as _select
-except ImportError:
-    from select import select as _select
+from errno import *
+from select import select, poll, epoll
+from select import error as SelectError
+from select import EPOLLIN, EPOLLOUT, EPOLLHUP, EPOLLERR
+from select import POLLIN, POLLOUT, POLLHUP, POLLERR, POLLNVAL
 
-from circuits import Event, Component
+from circuits.core import Event, BaseComponent
+
+_POLL_DISCONNECTED = (POLLHUP | POLLERR | POLLNVAL)
+_EPOLL_DISCONNECTED = (EPOLLHUP | EPOLLERR)
 
 TIMEOUT=0.00001
-READ = 1
-WRITE = 2
 
 ###
 ### Events
 ###
 
-class Error(Event):
-    """Error(fd) -> Error Event
+class Read(Event): pass
+class Write(Event): pass
+class Error(Event): pass
+class Disconnect(Event): pass
 
-    Error Event fired when an error occurs on a file descriptor.
-    """
+class _Poller(BaseComponent):
 
-class Read(Event):
-    """Read(fd) -> Read Event
+    channel = "poll"
 
-    Read Event fired when a file descriptor had data to be read.
-    """
+    def __init__(self, target, timeout=TIMEOUT, channel=channel):
+        super(_Poller, self).__init__(channel=channel)
 
-class Write(Event):
-    """Write(fd) -> Write Event
+        self.target = target
+        self.timeout = timeout
 
-    Write Event fired when a file descriptor is ready to be written to.
-    """
+        self._read = []
+        self._write = []
 
-class Poller(Component):
-    """Poller(...) -> new Poller Component
+    def addReader(self, fd):
+        self._read.append(fd)
 
-    Creates a new Poller Component that can be used to poll file descriptors
-    for read and write events. This is a base component and should be extended
-    to implement the desired poller implementation.
-    """
+    def addWriter(self, fd):
+        self._write.append(fd)
 
-    def __init__(self, *args, **kwargs):
-        super(Poller, self).__init__(*args, **kwargs)
+    def removeReader(self, fd):
+        self._read.remove(fd)
 
-        self._fds = []
+    def removeWriter(self, fd):
+        self._write.remove(fd)
 
-    def add(self, fd):
-        self._fds.append((fd, READ))
+    def isReading(self, fd):
+        return fd in self._read
 
-    def remove(self, fd):
-        self._fds.remove(fd)
+    def isWriting(self, fd):
+        return fd in self._write
 
-    @property
-    def read(self):
-        if hasattr(self, "_fds"):
-            return [fd for fd, flag in self._fds if flag & READ]
-        else:
-            return []
+    def discard(self, fd):
+        if fd in self._read:
+            self._read.remove(fd)
+        if fd in self._write:
+            self._write.remove(fd)
 
-    @property
-    def write(self):
-        if hasattr(self, "_fds"):
-            return [fd for fd, flag in self._fds if flag & WRITE]
-        else:
-            return []
+    def __tick__(self):
+        self.poll()
 
-    @property
-    def all(self):
-        if hasattr(self, "_fds"):
-            return [fd for fd, flag in self._fds if flag & (READ | WRITE)]
-        else:
-            return []
+    def poll(self):
+        raise NotImplemented
 
-class Select(Poller):
+class Select(_Poller):
     """Select(...) -> new Select Poller Component
 
     Creates a new Select Poller Component that uses the select poller
@@ -94,17 +86,218 @@ class Select(Poller):
     compatibility.
     """
 
-    def __tick__(self):
-        self.poll()
+    def _preenDescriptors(self):
+        for socks in (self._read[:], self._write[:]):
+            for sock in socks:
+                try:
+                    select([sock], [sock], [sock], 0)
+                except Exception, e:
+                    self.discard(sock)
 
-    def poll(self, timeout=TIMEOUT):
-        r, w, e = _select(self.read, self.write, self.all, timeout)
+    def poll(self):
+        try:
+            r, w, _ = select(self._read, self._write, [], self.timeout)
+        except ValueError, e:
+            # Possibly a file descriptor has gone negative?
+            return self._preenDescriptors()
+        except TypeError, e:
+            # Something *totally* invalid (object w/o fileno, non-integral
+            # result) was passed
+            return self._preenDescriptors()
+        except (SelectError, IOError), e:
+            # select(2) encountered an error
+            if e[0] in (0, 2):
+                # windows does this if it got an empty list
+                if (not self._read) and (not self._write):
+                    return
+                else:
+                    raise
+            elif e[0] == EINTR:
+                return
+            elif e[0] == EBADF:
+                return self._preenDescriptors()
+            else:
+                # OK, I really don't know what's going on.  Blow up.
+                raise
 
-        for fd in w:
-            self.push(Write(fd), "write", self.channel)
+        for sock in w:
+            self.send(Write(sock), "_write", self.target)
             
-        for fd in r:
-            self.push(Read(fd), "read", self.channel)
+        for sock in r:
+            self.send(Read(sock), "_read", self.target)
 
-        for fd in e:
-            self.push(Error(fd), "error", self.channel)
+class Poll(_Poller):
+    """Poll(...) -> new Poll Poller Component
+
+    Creates a new Poll Poller Component that uses the poll poller
+    implementation.
+    """
+
+
+    def __init__(self, *args, **kwargs):
+        super(Poll, self).__init__(*args, **kwargs)
+
+        if self.timeout is not None:
+            self.timeout = int(self.timeout * 1000)
+
+        self._map = {}
+        self._poller = poll()
+
+    def _updateRegistration(self, fd):
+        try:
+            fileno = fd.fileno()
+            self._poller.unregister(fileno)
+        except KeyError:
+            pass
+
+        mask = 0
+
+        if fd in self._read:
+            mask = mask | POLLIN
+        if fd in self._write:
+            mask = mask | POLLOUT
+
+        fileno = fd.fileno()
+
+        if mask:
+            self._poller.register(fd, mask)
+            self._map[fileno] = fd
+        else:
+            super(Poll, self).discard(fd)
+            del self._map[fileno]
+
+    def addReader(self, fd):
+        super(Poll, self).addReader(fd)
+        self._updateRegistration(fd)
+
+    def addWriter(self, fd):
+        super(Poll, self).addWriter(fd)
+        self._updateRegistration(fd)
+
+    def removeReader(self, fd):
+        super(Poll, self).addReader(fd)
+        self._updateRegistration(fd)
+
+    def removeWriter(self, fd):
+        super(Poll, self).addWriter(fd)
+        self._updateRegistration(fd)
+
+    def discard(self, fd):
+        super(Poll, self).discard(fd)
+        self._updateRegistration(fd)
+
+    def poll(self):
+        try:
+            l = self._poller.poll(self.timeout)
+        except SelectError, e:
+            if e[0] == EINTR:
+                return
+            else:
+                raise
+
+        for fileno, event in l:
+            self._process(fileno, event)
+
+    def _process(self, fileno, event):
+        fd = self._map[fileno]
+
+        if event & _POLL_DISCONNECTED and not (event & POLLIN):
+            self.discard(fd)
+            self.send(Disconnect(fd), "_disconnect", self.target)
+        else:
+            try:
+                if event & POLLIN:
+                    self.send(Read(fd), "_read", self.target)
+                if event & POLLOUT:
+                    self.send(Write(fd), "_write", self.target)
+            except Exception, e:
+                self.send(Error(fd, e), "_error", self.target)
+                self.send(Disconnect(fd), "_disconnect", self.target)
+                self.discard(fd)
+
+class EPoll(_Poller):
+    """EPoll(...) -> new EPoll Poller Component
+
+    Creates a new EPoll Poller Component that uses the epoll poller
+    implementation.
+    """
+
+    def __init__(self, *args, **kwargs):
+        super(EPoll, self).__init__(*args, **kwargs)
+
+        if self.timeout is not None:
+            self.timeout = int(self.timeout * 1000)
+
+        self._map = {}
+        self._poller = epoll()
+
+    def _updateRegistration(self, fd):
+        fileno = fd.fileno()
+
+        try:
+            self._poller.unregister(fileno)
+        except IOError:
+            pass
+
+        mask = 0
+
+        if fd in self._read:
+            mask = mask | EPOLLIN
+        if fd in self._write:
+            mask = mask | EPOLLOUT
+
+        if mask:
+            self._poller.register(fd, mask)
+            self._map[fileno] = fd
+        else:
+            super(EPoll, self).discard(fd)
+            del self._map[fileno]
+
+    def addReader(self, fd):
+        super(EPoll, self).addReader(fd)
+        self._updateRegistration(fd)
+
+    def addWriter(self, fd):
+        super(EPoll, self).addWriter(fd)
+        self._updateRegistration(fd)
+
+    def removeReader(self, fd):
+        super(EPoll, self).addReader(fd)
+        self._updateRegistration(fd)
+
+    def removeWriter(self, fd):
+        super(EPoll, self).addWriter(fd)
+        self._updateRegistration(fd)
+
+    def discard(self, fd):
+        super(EPoll, self).discard(fd)
+        self._updateRegistration(fd)
+
+    def poll(self):
+        try:
+            l = self._poller.poll(self.timeout)
+        except SelectError, e:
+            if e[0] == EINTR:
+                return
+            else:
+                raise
+
+        for fileno, event in l:
+            self._process(fileno, event)
+
+    def _process(self, fileno, event):
+        fd = self._map[fileno]
+
+        if event & _EPOLL_DISCONNECTED and not (event & POLLIN):
+            self.discard(fd)
+            self.send(Disconnect(fd), "_disconnect", self.target)
+        else:
+            try:
+                if event & EPOLLIN:
+                    self.send(Read(fd), "_read", self.target)
+                if event & EPOLLOUT:
+                    self.send(Write(fd), "_write", self.target)
+            except Exception, e:
+                self.send(Error(fd, e), "_error", self.target)
+                self.send(Disconnect(fd), "_disconnect", self.target)
+                self.discard(fd)
