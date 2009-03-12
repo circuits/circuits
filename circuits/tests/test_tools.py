f132302858bc715347b0026358966a758d41adc2
prologic <none@none>
2009-03-13 08:34:01 +1000

    tests.test_tools: (NEW) Added test suite for tools package (also re-tests the core)

---
+++ circuits/tests/test_tools.py
@@ -0,0 +1,122 @@
+# Module:   test_tools
+# Date:     13th March 2009
+# Author:   James Mills, prologic at shortcircuit dot net dot au
+
+"""Tools Test Suite
+
+Test all functionality of the tools package.
+"""
+
+import unittest
+
+from circuits import Event, Component
+from circuits.tools import kill, graph, inspect
+
+class A(Component):
+
+    def foo(self):
+        print "A!"
+
+class B(Component):
+
+    def foo(self):
+        print "B!"
+
+class C(Component):
+
+    def foo(self):
+        print "C!"
+
+class D(Component):
+
+    def foo(self):
+        print "D!"
+
+class E(Component):
+
+    def foo(self):
+        print "E!"
+
+class F(Component):
+
+    def foo(self):
+        print "F!"
+
+class TestKill(unittest.TestCase):
+    """Test sockets.TCPClient and sockets.TCPServer
+
+    Test that communication between a TCPClient and
+    TCPServer work correctly.
+    """
+
+    def runTest(self):
+        a = A()
+        b = B()
+        c = C()
+        d = D()
+        e = E()
+        f = F()
+
+        a += b
+        b += c
+
+        e += f
+        d += e
+        a += d
+
+        self.assertEquals(a.manager, a)
+        self.assertEquals(b.manager, a)
+        self.assertFalse(b._hidden)
+        self.assertEquals(c.manager, b)
+        self.assertFalse(c.components)
+        self.assertFalse(c._hidden)
+
+        self.assertTrue(b in a.components)
+        self.assertTrue(d in a.components)
+        self.assertTrue(c in a._hidden)
+        self.assertTrue(e in a._hidden)
+        self.assertTrue(f in a._hidden)
+
+        self.assertEquals(d.manager, a)
+        self.assertEquals(e.manager, d)
+        self.assertEquals(f.manager, e)
+
+        self.assertTrue(f in e.components)
+        self.assertTrue(e in d.components)
+        self.assertTrue(f in d._hidden)
+        self.assertFalse(f._components)
+        self.assertFalse(f._hidden)
+        self.assertFalse(e._hidden)
+
+        kill(d)
+
+        print inspect(a)
+
+        self.assertEquals(a.manager, a)
+        self.assertEquals(b.manager, a)
+        self.assertFalse(b._hidden)
+        self.assertEquals(c.manager, b)
+        self.assertFalse(c.components)
+        self.assertFalse(c._hidden)
+
+        self.assertTrue(b in a.components)
+        self.assertFalse(d in a.components)
+        self.assertFalse(e in d.components)
+        self.assertFalse(f in e.components)
+        self.assertTrue(c in a._hidden)
+        self.assertFalse(e in a._hidden)
+        #self.assertFalse(f in a._hidden) # Failing
+
+        self.assertEquals(d.manager, d)
+        self.assertEquals(e.manager, e)
+        self.assertEquals(f.manager, f)
+
+        self.assertFalse(d.components)
+        self.assertFalse(e.components)
+        self.assertFalse(f.components)
+        self.assertFalse(d._hidden)
+        self.assertFalse(e._hidden)
+        self.assertFalse(f._hidden)
+
+if __name__ == "__main__":
+    unittest.main()
