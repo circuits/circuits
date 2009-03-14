9cb819223df1f89fea1b9cfa8fa34bb4440f0a04
prologic <none@none>
2009-03-15 01:26:40 +1000

    tools.tests.test_tools: Removed inspect test as this cannot be tested for reliably

--- circuits/tools/tests/test_tools.py
+++ circuits/tools/tests/test_tools.py
@@ -68,29 +68,6 @@ GRAPH = """\
   * <E/* (q: 1 c: 1 h: 2) [S]>
    * <F/* (q: 0 c: 1 h: 1) [S]>"""
 
-INSPECT = """\
- Registered Components: 2
-  <D/* (q: 1 c: 1 h: 3) [S]>
-  <B/* (q: 0 c: 1 h: 2) [S]>
-
- Tick Functions: 6
-  <bound method F.__tick__ of <F/* (q: 0 c: 1 h: 1) [S]>>
-  <bound method B.__tick__ of <B/* (q: 0 c: 1 h: 2) [S]>>
-  <bound method C.__tick__ of <C/* (q: 0 c: 1 h: 1) [S]>>
-  <bound method E.__tick__ of <E/* (q: 1 c: 1 h: 2) [S]>>
-  <bound method D.__tick__ of <D/* (q: 1 c: 1 h: 3) [S]>>
-  <bound method A.__tick__ of <A/* (q: 3 c: 1 h: 6) [S]>>
-
- Channels and Event Handlers: 1
-  *:foo; 6
-   <handler ('foo',) {filter: False, target: None) of <F/* (q: 0 c: 1 h: 1) [S]>>
-   <handler ('foo',) {filter: False, target: None) of <D/* (q: 1 c: 1 h: 3) [S]>>
-   <handler ('foo',) {filter: False, target: None) of <B/* (q: 0 c: 1 h: 2) [S]>>
-   <handler ('foo',) {filter: False, target: None) of <A/* (q: 3 c: 1 h: 6) [S]>>
-   <handler ('foo',) {filter: False, target: None) of <C/* (q: 0 c: 1 h: 1) [S]>>
-   <handler ('foo',) {filter: False, target: None) of <E/* (q: 1 c: 1 h: 2) [S]>>
-"""
-
 class TestKill(unittest.TestCase):
     """Test kill() function
 
@@ -189,46 +166,6 @@ class TestGraph(unittest.TestCase):
         self.assertFalse(f._components)
 
         self.assertEquals(graph(a), GRAPH)
-
-class TestInspect(unittest.TestCase):
-    """Test inspect() function
-
-    Test the inspect function and ensure that the represented structure of x
-    and it's registered components and hidden components are correct.
-    """
-
-    def runTest(self):
-        a = A()
-        b = B()
-        c = C()
-        d = D()
-        e = E()
-        f = F()
-
-        a += b
-        b += c
-
-        e += f
-        d += e
-        a += d
-
-        self.assertEquals(a.manager, a)
-        self.assertEquals(b.manager, a)
-        self.assertEquals(c.manager, b)
-        self.assertFalse(c.components)
-
-        self.assertTrue(b in a.components)
-        self.assertTrue(d in a.components)
-
-        self.assertEquals(d.manager, a)
-        self.assertEquals(e.manager, d)
-        self.assertEquals(f.manager, e)
-
-        self.assertTrue(f in e.components)
-        self.assertTrue(e in d.components)
-        self.assertFalse(f._components)
-
-        self.assertEquals(inspect(a), INSPECT)
 
 if __name__ == "__main__":
     unittest.main()
