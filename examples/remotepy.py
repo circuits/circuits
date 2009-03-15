700b0a76408f1f44179973bdbac15073c9490eae
prologic <none@none>
2009-03-15 19:37:25 +1000

    examples/remote.py: Fixed this example (if only the Bridge works)

--- examples/remotepy.py
+++ examples/remotepy.py
@@ -12,23 +12,24 @@ computes the expression, packages up the result and sends it back across
 the event bridge as an event.
 
 This example demonstrates:
-	* Basic Request/Response model using events.
-	* Briding systems/components.
+    * Basic Request/Response model using events.
+    * Briding systems/components.
 
 This example makes use of:
-	* Component
-	* Bridge
-	* Manager
-	* Stdin (lib)
+    * Component
+    * Bridge
+    * Manager
+    * Stdin (lib)
 """
 
-import sys
 import optparse
 
 import circuits
-from circuits import Debugger
-from circuits import listener, Event, Component, Bridge, Manager
-from circuits.lib.io import Stdin
+from circuits import io
+from circuits.core import bridge
+from circuits import handler, Event, Component, Debugger, Manager
+
+from circuits.tools import graph, inspect
 
 USAGE = "%prog [options] [host[:port]]"
 VERSION = "%prog v" + circuits.__version__
@@ -42,42 +43,42 @@ ERRORS[1] = "Only specify one of either server (-s/--server) mode or client (-c/
 ###
 
 def parse_options():
-	"""parse_options() -> opts, args
+    """parse_options() -> opts, args
 
-	Parse the command-line options given returning both
-	the parsed options and arguments.
-	"""
+    Parse the command-line options given returning both
+    the parsed options and arguments.
+    """
 
-	parser = optparse.OptionParser(usage=USAGE, version=VERSION)
+    parser = optparse.OptionParser(usage=USAGE, version=VERSION)
 
-	parser.add_option("-s", "--server",
-			action="store_true", default=False, dest="server",
-			help="Server mode")
+    parser.add_option("-s", "--server",
+            action="store_true", default=False, dest="server",
+            help="Server mode")
 
-	parser.add_option("-c", "--client",
-			action="store_true", default=False, dest="client",
-			help="Client mode")
+    parser.add_option("-c", "--client",
+            action="store_true", default=False, dest="client",
+            help="Client mode")
 
-	parser.add_option("-b", "--bind",
-			action="store", type="string", default="0.0.0.0:8000", dest="bind",
-			help="Bind to address:port")
+    parser.add_option("-b", "--bind",
+            action="store", type="string", default="0.0.0.0:8000", dest="bind",
+            help="Bind to address:port")
 
-	parser.add_option("-d", "--debug",
-			action="store_true", default=False, dest="debug",
-			help="Enable debug mode")
+    parser.add_option("-d", "--debug",
+            action="store_true", default=False, dest="debug",
+            help="Enable debug mode")
 
-	opts, args = parser.parse_args()
+    opts, args = parser.parse_args()
 
-	if not (opts.server or opts.client):
-		print "ERROR: %s" % ERRORS[0]
-		parser.print_help()
-		raise SystemExit, 1
-	elif opts.server and opts.client:
-		print "ERROR: %s" % ERRORS[1]
-		parser.print_help()
-		raise SystemExit, 2
+    if not (opts.server or opts.client):
+        print "ERROR: %s" % ERRORS[0]
+        parser.print_help()
+        raise SystemExit, 1
+    elif opts.server and opts.client:
+        print "ERROR: %s" % ERRORS[1]
+        parser.print_help()
+        raise SystemExit, 2
 
-	return opts, args
+    return opts, args
 
 ###
 ### Events
@@ -90,91 +91,77 @@ class NewInput(Event): pass
 ### Components
 ###
 
-class Input(Stdin):
+class Input(Component):
 
-	@listener("read")
-	def onREAD(self, data):
-		self.push(NewInput(data.strip()), "newinput")
+    def __init__(self):
+        super(Input, self).__init__()
+
+        self.push(io.Write(">>> "), "write", "stdout")
+
+    @handler("read", target="stdin")
+    def read(self, data):
+        self.push(NewInput(data.strip()), "newinput")
+        self.push(io.Write(">>> "), "write", "stdout")
 
 class Calc(Component):
 
-	@listener("result")
-	def onRESULT(self, r):
-		sys.stdout.write("%s\n" % r)
-		sys.stdout.write(">>> ")
-		sys.stdout.flush()
+    def result(self, r):
+        self.push(io.Write("%s\n" % r), "write", "stdout")
 
 class Adder(Component):
 
-	@listener("newinput")
-	def onNEWINPUT(self, s):
-		print "Evaluating: %s" % s
-		r = eval(s)
-		self.push(Result(r), "result")
+    def newinput(self, s):
+        print "Evaluating: %s" % s
+        r = eval(s)
+        self.push(Result(r), "result")
 
 ###
 ### Main
 ###
 
 def main():
-	opts, args = parse_options()
+    opts, args = parse_options()
 
-	if ":" in opts.bind:
-		address, port = opts.bind.split(":")
-		port = int(port)
-	else:
-		address, port = opts.bind, 8000
+    if ":" in opts.bind:
+        address, port = opts.bind.split(":", 1)
+        bind = (address, int(port))
+    else:
+        bind = (opts.bind, 8000)
 
-	if args:
-		x = args[0].split(":")
-		if len(x) > 1:
-			nodes = [(x[0], int(x[1]))]
-		else:
-			nodes = [(x[0], 8000)]
-	else:
-		nodes = []
+    nodes = []
+    for arg in args:
+        if ":" in arg:
+            address, port = arg.split(":", 1)
+            port = int(port)
+        else:
+            address, port = arg, bind[1]
+        nodes.append((address, port))
 
-	manager = Manager()
+    manager = Manager() + bridge.Bridge(bind, nodes)
 
-	bridge = Bridge(port, address=address, nodes=nodes)
-	manager += bridge
+    if opts.debug:
+        debugger = Debugger()
+        debugger.IgnoreEvents.extend(
+                [bridge.Read, bridge.Write, bridge.Error, bridge.Close,
+                    io.Read, io.Write, io.Error])
+        manager += debugger
 
-	if opts.debug:
-		debugger = Debugger()
-		debugger.IgnoreEvents.extend(["Read", "Write"])
-		manager += debugger
+    if opts.server:
+        manager += Adder()
+    elif opts.client:
+        manager += io.stdin
+        manager += io.stdout
+        manager += Input()
+        manager += Calc()
 
-	if opts.server:
-		manager += Adder()
+    print graph(manager)
+    print inspect(manager)
 
-		while True:
-			try:
-				manager.flush()
-				bridge.poll()
-			except KeyboardInterrupt:
-				break
-	elif opts.client:
-		input = Input()
-		manager += input
-		manager += Calc()
-
-		sys.stdout.write(">>> ")
-		sys.stdout.flush()
-
-		while True:
-			try:
-				manager.flush()
-				input.poll()
-				bridge.poll()
-			except KeyboardInterrupt:
-				break
-	else:
-		print "ERROR: Invalid mode"
-		raise SystemExit, 255
+    manager.run()
 
 ###
 ### Entry Point
 ###
 
 if __name__ == "__main__":
-	main()
+    main()
