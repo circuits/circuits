e4e6dd2658af361eb8ce82cd7a3c9bdd4fd73612
prologic <none@none>
2009-03-16 08:00:56 +1000

    core.bridge: New prototype bridge that works and reuses networking and poller components rather then implementing it's own transport

--- circuits/core/bridge.py
+++ circuits/core/bridge.py
@@ -19,97 +19,56 @@ uses the UDP protocol and as such events cannot be guaranteed of their
 order or delivery.
 """
 
-import socket
-import select
-from cPickle import dumps as pickle
-from cPickle import loads as unpickle
+from cPickle import dumps, loads
 from socket import gethostname, gethostbyname
 
 from circuits import handler, Event, Component
+from circuits.net.sockets import UDPServer as DefaultTransport
+from circuits.net.sockets import Read, Write, Error, Close
 
-
-POLL_INTERVAL = 0.00001
-BUFFER_SIZE = 131072
-
-
-class Read(Event): pass
 class Helo(Event): pass
-class Error(Event): pass
-class Write(Event): pass
-class Close(Event): pass
-
 
 class Bridge(Component):
 
-    IgnoreEvents = ["Read", "Write"]
+    channel = "bridge"
+
+    IgnoreEvents = [Read, Write, Error, Close]
     IgnoreChannels = []
 
-    def __init__(self, port, address="", nodes=[], *args, **kwargs):
-        super(Bridge, self).__init__(*args, **kwargs)
-
-        self._sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self._sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self._sock.setsockopt(socket.SOL_SOCKET,    socket.SO_BROADCAST, 1)
-        self._sock.setblocking(False)
-        self._sock.bind((address, port))
-
-        self._write = []
-        self._buffers = {}
-        self._read = [self._sock]
-
-        self.address = address
-        self.port = port
+    def __init__(self, nodes=[], **kwargs):
+        super(Bridge, self).__init__(**kwargs)
 
         self.nodes = set(nodes)
 
-        if self.address in ["", "0.0.0.0"]:
-            address = gethostbyname(gethostname())
+        if "transport" in kwargs:
+            if isinstance(kwargs["transport"], Component):
+                self.transport = kwargs["transport"]
+            elif issubclass(kwargs["transport"], Component):
+                Transport = kwargs["transport"]
+                kwargs["channel"] = "transport"
+                self.transport = Transport(**kwargs)
+            else:
+                raise TypeError("Invalid transport")
         else:
-            address = self.address
+            kwargs["channel"] = "transport"
+            self.transport = DefaultTransport(**kwargs)
 
-        self.ourself = (address, self.port)
+        self += self.transport
 
-    def registered(self, component, manager):
-        self.push(Helo(*self.ourself), "helo")
+        if self.transport.address in ["", "0.0.0.0"]:
+            address = gethostbyname(gethostname())
+            self.ourself = (address, self.transport.port)
+        else:
+            self.ourself = self.transport.bind
 
-    def __write__(self, address, data):
-        if not self._write:
-            self._write.append(self._sock)
-        if not self._buffers.has_key(address):
-            self._buffers[address] = []
-        self._buffers[address].append(data)
+    def registered(self, c, m):
+        if c == self:
+            self.push(Helo(*self.ourself), "helo")
 
-    def __close__(self):
-        self.push(Close(), "close", self.channel)
-
-    def __tick__(self):
-        self.poll()
-
-    def poll(self, wait=POLL_INTERVAL):
-        r, w, e = select.select(self._read, self._write, [], wait)
-
-        if w:
-            for address, data in self._buffers.iteritems():
-                if data:
-                    self.send(Write(address, data[0]), "write", self.channel)
-            self._write.remove(w[0])
-
-        if r:
-            try:
-                data, address = self._sock.recvfrom(BUFFER_SIZE)
-
-                if not data:
-                    self.__close__()
-                else:
-                    self.push(Read(address, data), "read", self.channel)
-            except socket.error, e:
-                self.push(Error(self._sock, e), "error", self.channel)
-                self.__close__()
-
-    @handler(filter=True)
-    def onEVENTS(self, event, *args, **kwargs):
+    @handler(filter=True, priority=100)
+    def event(self, event, *args, **kwargs):
         channel = event.channel
-        if True in [event.name == name for name in self.IgnoreEvents]:
+        if True in [event.name == x.__name__ for x in self.IgnoreEvents]:
             return
         elif channel in self.IgnoreChannels:
             return
@@ -119,19 +78,35 @@ class Bridge(Component):
             event.ignore = True
 
         event.source = self.ourself
+
         try:
-            s = pickle(event, -1)
+            s = dumps(event, -1)
         except:
             return
 
         if self.nodes:
             for node in self.nodes:
-                self.__write__(node, s)
+                self.transport.write(node, s)
         else:
-            self.__write__(("<broadcast>", self.port), s)
+            target, channel = channel
+            if type(target) is tuple:
+                if type(target[0]) is int:
+                    node = ("0.0.0.0", target[0])
+                elif type(target[0]) is str and ":" in target[0]:
+                    address, port = target[0].split(":", 1)
+                    port = int(port)
+                    node = (address, port)
+                elif type(target[0]) is str:
+                    node = (node, self.transport.port)
+                else:
+                    raise TypeError("Invalid bridge target!")
+            else:
+                node = ("0.0.0.0", self.transport.port)
+
+            self.transport.write(node, s)
 
     @handler("helo", filter=True)
-    def onHELO(self, event, address, port):
+    def helo(self, event, address, port):
         source = event.source
 
         if (address, port) == self.ourself or source in self.nodes:
@@ -139,11 +114,11 @@ class Bridge(Component):
 
         if not (address, port) in self.nodes:
             self.nodes.add((address, port))
-            self.push(Helo(*self.ourself), "helo")
+            self.push(Helo(*self.ourself))
 
-    @handler("read", filter=True)
-    def onREAD(self, address, data):
-        event = unpickle(data)
+    @handler("read", filter=True, target="transport")
+    def read(self, address, data):
+        event = loads(data)
 
         (target, channel) = event.channel
         source = event.source
@@ -151,40 +126,10 @@ class Bridge(Component):
         if source == self.ourself:
             return
 
-        self.send(event, channel, target)
-
-    @handler("close", filter=True)
-    def onCLOSE(self):
-        """Close Event
-
-        Typically this should NOT be overridden by sub-classes.
-        If it is, this should be called by the sub-class first.
-        """
-
-        try:
-            self._read.remove(self._sock)
-            self._write.remove(self._sock)
-            self._sock.shutdown(2)
-            self._sock.close()
-        except socket.error, error:
-            self.push(Error(error), "error", self.channel)
-
-    @handler("write", filter=True)
-    def onWRITE(self, address, data):
-        """Write Event
-
-
-        Typically this should NOT be overridden by sub-classes.
-        If it is, this should be called by the sub-class first.
-        """
-
-        try:
-            self._sock.sendto(data, address)
-            del self._buffers[address][0]
-        except socket.error, e:
-            if e[0] in [32, 107]:
-                self.__close__()
+        if type(target) is tuple:
+            if len(target) == 2:
+                target = target[1]
             else:
-                self.push(Error(e), "error", self.channel)
-                self.__close__()
+                target = "*"
 
+        self.send(event, channel, target)
