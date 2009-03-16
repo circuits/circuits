11b2c260d02497c105bb25241836da8680f4e092
prologic <none@none>
2009-03-17 02:51:17 +1000

    web.dispatchers: (NEW) Prototype VirtualHosts dispatcher

--- circuits/web/dispatchers.py
+++ circuits/web/dispatchers.py
@@ -8,6 +8,7 @@ This module implements URL dispatchers.
 """
 
 import os
+from urlparse import urljoin as _urljoin
 
 from circuits import handler, Component
 
@@ -176,3 +177,47 @@ class Dispatcher(Component):
                 req.args += tuple(vpath)
 
             return self.send(req, channel, target, errors=True)
+
+class VirtualHosts(Component):
+    """Forward to anotehr Dispatcher based on the Host header.
+    
+    This can be useful when running multiple sites within one server.
+    It allows several domains to point to different parts of a single
+    website structure. For example:
+     - http://www.domain.example      -> /
+     - http://www.domain2.example     -> /domain2
+     - http://www.domain2.example:443 -> /secure
+    
+    @param domains: a dict of {host header value: virtual prefix} pairs.
+    @type  domains: dict
+
+    The incoming "Host" request header is looked up in this dict,
+    and, if a match is found, the corresponding "virtual prefix"
+    value will be prepended to the URL path before passing the
+    request onto the next dispatcher.
+
+    Note that you often need separate entries for "example.com"
+    and "www.example.com". In addition, "Host" headers may contain
+    the port number.
+    """
+
+    channel = "web"
+
+    def __init__(self, **domains):
+        super(Dispatcher, self).__init__()
+
+        self.domains = domains
+
+    @handler("request", filter=True)
+    def request(self, event, request, response):
+        req = event
+        path = request.path.strip("/")
+
+        header = req.headers.get
+        domain = header("X-Forwarded-Host", header("Host", ""))
+        prefix = self.domains.get(domain, "")
+        if prefix:
+            path = _urljoin(prefix, path)
+        request.path = path
+        
+        return self.send(req, "request")
