4cb77b9e12b8f4e2231d96accdd4752af6e8df0a
prologic <none@none>
2009-06-22 13:08:03 +1000

    examples/helloweb.py: Patched to work with 533248273a4a

--- examples/helloweb.py
+++ examples/helloweb.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 
 from circuits import Component
-from circuits.web import BaseServer
+from circuits.web import BaseServer, Response
 
 class Root(Component):
 
     def request(self, request, response):
-        return "Hello World!"
+        response.body = "Hello World!"
+        self.push(Response(response))
 
 (BaseServer(8000) + Root()).run()
