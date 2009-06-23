f217a577c901c199ece1ade1358d5d89d8be63f4
prologic <none@none>
2009-06-24 00:48:35 +1000

    tmp/test_feedback.py: Use the Smarter Event object implemented in 070fc11bf615 as an improved example

--- tmp/test_feedback.py
+++ tmp/test_feedback.py
@@ -1,11 +1,21 @@
 #!/usr/bin/python -i
 
-from circuits import Event, Component, Debugger, Manager
+from circuits import Event, Component
 
-class Foo(Component):
+class Foo(Event):
+    success = "foo_success"
+    failure = "foo_failure"
+
+class System(Component):
 
     def foo(self):
         return "foo"
 
-m = Manager() + Foo() + Debugger()
-m.start()
+    def foo_success(self, r):
+        print "foo: %s" % repr(r)
+
+    def foo_failure(self, e):
+        print "foo: %s" % str(e)
+
+sys = System()
+sys.start()
