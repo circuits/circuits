df370b9c0cb90cb092105523f5f4ea7fe7d8ee2b
prologic <none@none>
2009-06-24 23:49:33 +1000

    setup.py: Fixed distutils setup - Closes #55

--- setup.py
+++ setup.py
@@ -1,9 +1,35 @@
 #!/usr/bin/env python
 
+
 try:
-    from setuptools import setup
+    from setuptools import setup, find_packages
+    HAS_SETUPTOOLS = True
 except ImportError:
     from distutils.core import setup
+    HAS_SETUPTOOLS = False
+
+if not HAS_SETUPTOOLS:
+    import os
+
+    def is_package(path):
+        return (
+            os.path.isdir(path) and
+            os.path.isfile(os.path.join(path, '__init__.py')
+            ))
+
+    def find_packages(path, base=""):
+        """ Find all packages in path """
+        packages = {}
+        for item in os.listdir(path):
+            dir = os.path.join(path, item)
+            if is_package( dir ):
+                if base:
+                    module_name = "%(base)s.%(item)s" % vars()
+                else:
+                    module_name = item
+                packages[module_name] = dir
+                packages.update(find_packages(dir, module_name))
+        return packages
 
 from circuits import version
 version.forget_version()
@@ -26,7 +52,7 @@ setup(
 		license=pkg.__license__,
 		keywords=pkg.__keywords__,
 		platforms=pkg.__platforms__,
-		packages=[pkg.__name__],
+		packages=find_packages("."),
 		install_requires=pkg.__install_requires__,
 		setup_requires=pkg.__setup_requires__,
 		extras_require=pkg.__extras_require__,
