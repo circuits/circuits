dbba5aae57bef68d6699a5b5292ea2c35c3a2f32
prologic <none@none>
2009-07-02 13:34:26 +1000

    examples/web/makotemplates.py: Added basic example of using mako as a templating engine with circuits.web

---
+++ examples/web/makotemplates.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import os
+
+import mako
+from mako.lookup import TemplateLookup
+
+from circuits.web import Server, Controller
+
+DEFAULTS = {}
+
+templates = TemplateLookup(
+	directories=[os.path.join(os.path.dirname(__file__), "tpl")],
+	module_directory="/tmp",
+	output_encoding="utf-8")
+
+def render(name, **d):
+	try:
+		d.update(DEFAULTS)
+		tpl = templates.get_template(name)
+		return tpl.render(**d)
+	except:
+		return mako.exceptions.html_error_template().render()
+
+class Root(Controller):
+
+	tpl = "index.html"
+
+	def index(self):
+		return render(self.tpl)
+
+	def submit(self, firstName, lastName):
+		msg = "Thank you %s %s" % (firstName, lastName)
+		return render(self.tpl, message=msg)
+
+(Server(8000) + Root()).run()
