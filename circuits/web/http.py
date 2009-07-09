122464307c1257e61fc8e273fb8165bcfca27b6e
prologic <none@none>
2009-07-10 07:23:55 +1000

    web.http: Only listen for "request_success" events (removed request_filtered - not needed). Set the resposne.body to retval (regardless of what it is).

--- circuits/web/http.py
+++ circuits/web/http.py
@@ -219,18 +219,17 @@ class HTTP(Component):
 
         self.push(Response(response), "response", self.channel)
 
-    @handler("request_success", "request_filtered")
-    def request_success_or_filtered(self, evt, handler, retval):
+    @handler("request_success")
+    def request_success(self, evt, handler, retval):
         request, response = evt.args[:2]
-        if retval:
-            request.handled = True
-            if issubclass(type(retval), basestring):
-                response.body = retval
-                self.push(Response(response), "response", self.channel)
-            elif isinstance(retval, HTTPError):
-                self._handleError(retval)
-            elif isinstance(retval, wrappers.Response):
-                self.push(Response(retval), "response", self.channel)
+        request.handled = True
+        if isinstance(retval, HTTPError):
+            self._handleError(retval)
+        elif isinstance(retval, wrappers.Response):
+            self.push(Response(retval), "response", self.channel)
+        else:
+            response.body = retval
+            self.push(Response(response), "response", self.channel)
 
     def request_failure(self, evt, handler, error):
         request, response = evt.args[:2]
