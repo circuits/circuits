ab06e871a6c820845da2d4d60bb6b1874350cf16
prologic <none@none>
2009-07-29 05:33:45 +1000

    circuits.web: Add url and expose to this namesapce

--- circuits/web/__init__.py
+++ circuits/web/__init__.py
@@ -8,9 +8,10 @@ circuits.web contains the circuits full stack web server that is HTTP
 and WSGI compliant.
 """
 
+from utils import url
 from loggers import Logger
-from core import Controller
 from sessions import Sessions
+from core import expose, Controller
 from events import Request, Response
 from servers import BaseServer, Server
 from errors import HTTPError, Forbidden, NotFound, Redirect
