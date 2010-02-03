5905f30ba10a517d3f837253b1e65159f42c052b
prologic <none@none>
2010-02-04 04:03:08 +1000

    circuits.net.protocols: Import irc, smtp and line protocols into the circuits.net.protocols namespace

---
+++ circuits/net/protocols/__init__.py
@@ -0,0 +1,12 @@
+# Package:  protocols
+# Date:     13th March 2009
+# Author:   James Mills, prologic at shortcircuit dot net dot au
+
+"""Networking Protocols
+
+This package contains components that implement various networking protocols.
+"""
+
+from line import LP
+from irc import IRC
+from smtp import SMTP
