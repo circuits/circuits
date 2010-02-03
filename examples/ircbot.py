3e8c85921b4e4f3f6d93b2083881e8e19ac64a6c
prologic <none@none>
2010-02-04 08:43:52 +1000

    examples/ircbot.py:  * Fixed example - use of __call__ (callable components) has been deprecated for some time. Instead keep a reference to the IRC Protocol Component.  * Changed default nick and name to reflect that it's a circuits IRC Bot.  * Make bot join #circuits by default.

--- examples/ircbot.py
+++ examples/ircbot.py
@@ -2,22 +2,26 @@
 
 from circuits import Component, Debugger
 from circuits.net.sockets import TCPClient, Connect
-from circuits.net.protocols.irc import IRC, Message, User, Nick
+from circuits.net.protocols.irc import IRC, Message, User, Nick, Join
 
 class Bot(Component):
 
     def __init__(self, host, port=6667, channel=None):
         super(Bot, self).__init__(channel=channel)
-        self += TCPClient(channel=channel) + IRC(channel=channel)
+        self.client = TCPClient(channel=channel)
+        self.irc = IRC(channel=channel)
+        self.irc.register(self.client)
+        self.client.register(self)
         self.push(Connect(host, port), "connect")
 
     def connected(self, host, port):
-        self.push(User("test", host, host, "Test Bot"), "USER")
-        self.push(Nick("test"), "NICK")
+        self.push(User("circuits", host, host, "Test circuits IRC Bot"), "USER")
+        self.push(Nick("circuits"), "NICK")
+        self.push(Join("#circuits"), "JOIN")
 
     def numeric(self, source, target, numeric, args, message):
         if numeric == 433:
-            self.push(Nick("%s_" % self("getNick")), "NICK")
+            self.push(Nick("%s_" % self.irc.getNick()), "NICK")
 
     def message(self, source, target, message):
         self.push(Message(source[0], message), "PRIVMSG")
