567e233030b6310347851e9808f0ceebdb57e451
prologic <none@none>
2010-02-05 08:31:40 +1000

    circuits.net.protocols.smtp: Fixed initial state function of __states dict

--- circuits/net/protocols/smtp.py
+++ circuits/net/protocols/smtp.py
@@ -1,6 +1,6 @@
-# Module:	smtp
-# Date:		13th June 2008
-# Author:	James Mills, prologic at shortcircuit dot net dot au
+# Module:   smtp
+# Date:     13th June 2008
+# Author:   James Mills, prologic at shortcircuit dot net dot au
 
 """Simple Mail Transfer Protocol
 
@@ -20,42 +20,42 @@ from circuits import handler, Event, Component
 ###
 
 def stripAddress(address):
-	"""stripAddress(address) -> address
+    """stripAddress(address) -> address
 
-	Strip the leading & trailing <> from an address.
-	Handy for getting FROM: addresses.
-	"""
+    Strip the leading & trailing <> from an address.
+    Handy for getting FROM: addresses.
+    """
 
-	start = address.index("<") + 1
-	end = address.index(">")
+    start = address.index("<") + 1
+    end = address.index(">")
 
-	return address[start:end]
+    return address[start:end]
 
 def splitTo(address):
-	"""splitTo(address) -> (host, fulladdress)
+    """splitTo(address) -> (host, fulladdress)
 
-	Return 'address' as undressed (host, fulladdress) tuple.
-	Handy for use with TO: addresses.
-	"""
+    Return 'address' as undressed (host, fulladdress) tuple.
+    Handy for use with TO: addresses.
+    """
 
-	start = address.index("<") + 1
-	sep = address.index("@") + 1
-	end = address.index(">")
+    start = address.index("<") + 1
+    sep = address.index("@") + 1
+    end = address.index(">")
 
-	return (address[sep:end], address[start:end],)
+    return (address[sep:end], address[start:end],)
 
 def getAddress(keyword, arg):
-	address = None
-	keylen = len(keyword)
-	if arg[:keylen].upper() == keyword:
-		address = arg[keylen:].strip()
-		if not address:
-			pass
-		elif address[0] == "<" and address[-1] == ">" and address != "<>":
-			# Addresses can be in the form <person@dom.com> but watch out
-			# for null address, e.g. <>
-			address = address[1:-1]
-	return address
+    address = None
+    keylen = len(keyword)
+    if arg[:keylen].upper() == keyword:
+        address = arg[keylen:].strip()
+        if not address:
+            pass
+        elif address[0] == "<" and address[-1] == ">" and address != "<>":
+            # Addresses can be in the form <person@dom.com> but watch out
+            # for null address, e.g. <>
+            address = address[1:-1]
+    return address
 
 ###
 ### Events
@@ -75,29 +75,29 @@ class Message(Event): pass
 ###
 
 class SMTP(Component):
-	"""SMTP(event) -> new smtp object
+    """SMTP(event) -> new smtp object
 
-	Create a new smtp object which implements the SMTP
-	protocol. Note this doesn't actually do anything
-	usefull unless used in conjunction with
-	circuits.parts.sockets.TCPServer.
+    Create a new smtp object which implements the SMTP
+    protocol. Note this doesn't actually do anything
+    usefull unless used in conjunction with
+    circuits.parts.sockets.TCPServer.
 
-	See: examples/net/smtpd.py
-	"""
+    See: examples/net/smtpd.py
+    """
 
-	COMMAND = 0
-	DATA = 1
+    COMMAND = 0
+    DATA = 1
 
-	__buffers = defaultdict(str)
+    __buffers = defaultdict(str)
 
-	__states = defaultdict(COMMAND)
+    __states = defaultdict(lambda : COMMAND)
 
-	__greeting = None
-	__mailfrom = None
-	__rcpttos = []
-	__data = None
+    __greeting = None
+    __mailfrom = None
+    __rcpttos = []
+    __data = None
 
-	__fqdn = socket.getfqdn()
+    __fqdn = socket.getfqdn()
 
     def __init__(self, *args, **kwargs):
         super(SMTP, self).__init__(*args, **kwargs)
@@ -105,193 +105,193 @@ class SMTP(Component):
         LP(getBuffer=self.__buffers.__getitem__,
                 updateBuffer=self.__buffers.__setitem__)
 
-	###
-	### Methods
-	###
+    ###
+    ### Methods
+    ###
 
-	def reset(self):
-		self.__buffers = {}
-		self.__states = {}
-		self.__greeting = None
-		self.__mailfrom = None
-		self.__rcpttos = []
-		self.__data = None
+    def reset(self):
+        self.__buffers = {}
+        self.__states = {}
+        self.__greeting = None
+        self.__mailfrom = None
+        self.__rcpttos = []
+        self.__data = None
 
-	def processMessage(self, sock, mailfrom, rcpttos, data):
-		r =  self.send(Message(sock, mailfrom, rcpttos, data), "message")
-		data.close()
+    def processMessage(self, sock, mailfrom, rcpttos, data):
+        r =  self.send(Message(sock, mailfrom, rcpttos, data), "message")
+        data.close()
 
-		if r:
-			return r[-1]
+        if r:
+            return r[-1]
 
-	###
-	### Properties
-	###
+    ###
+    ### Properties
+    ###
 
-	def getFQDN(self):
-		"""I.getFQDN() -> str
+    def getFQDN(self):
+        """I.getFQDN() -> str
 
-		Return the fully qualified domain name of this server.
-		"""
+        Return the fully qualified domain name of this server.
+        """
 
-		return self.__fqdn
+        return self.__fqdn
 
-	###
-	### Event Processing
-	###
+    ###
+    ### Event Processing
+    ###
 
-	@handler("raw")
-	def onRAW(self, sock, line):
-		"""I.onRAW(sock, line) -> None
+    @handler("raw")
+    def onRAW(self, sock, line):
+        """I.onRAW(sock, line) -> None
 
-		Process a line of text and generate the appropiate
-		event. This must not be overridden by sub-classes,
-		if it is, this must be explitetly called by the
-		sub-class. Other Components may however listen to
-		this event and process custom SMTP events.
-		"""
+        Process a line of text and generate the appropiate
+        event. This must not be overridden by sub-classes,
+        if it is, this must be explitetly called by the
+        sub-class. Other Components may however listen to
+        this event and process custom SMTP events.
+        """
 
-		if self.__states[sock] == self.COMMAND:
-			if not line:
-				self.write(sock, "500 Syntax error, command unrecognized\r\n")
-				return
+        if self.__states[sock] == self.COMMAND:
+            if not line:
+                self.write(sock, "500 Syntax error, command unrecognized\r\n")
+                return
 
-			method = None
+            method = None
 
-			i = line.find(" ")
-			if i < 0:
-				command = line.upper()
-				arg = None
-			else:
-				command = line[:i].upper()
-				arg = line[i+1:].strip()
+            i = line.find(" ")
+            if i < 0:
+                command = line.upper()
+                arg = None
+            else:
+                command = line[:i].upper()
+                arg = line[i+1:].strip()
 
-			method = getattr(self, "smtp" + command, None)
+            method = getattr(self, "smtp" + command, None)
 
-			if not method:
-				self.write(sock, "502 Command not implemented\r\n")
-			else:
-				method(sock, arg)
-		else:
-			if self.__states[sock] != self.DATA:
-				self.write(sock, "451 Internal confusion\r\n")
-				return
+            if not method:
+                self.write(sock, "502 Command not implemented\r\n")
+            else:
+                method(sock, arg)
+        else:
+            if self.__states[sock] != self.DATA:
+                self.write(sock, "451 Internal confusion\r\n")
+                return
 
-			if line and re.match("^\.$", line):
-					self.__data.flush()
-					self.__data.seek(0)
-					status = self.processMessage(sock,
-							self.__mailfrom,  self.__rcpttos, self.__data)
+            if line and re.match("^\.$", line):
+                    self.__data.flush()
+                    self.__data.seek(0)
+                    status = self.processMessage(sock,
+                            self.__mailfrom,  self.__rcpttos, self.__data)
 
-					self.reset()
+                    self.reset()
 
-					if not status:
-						self.write(sock, "250 Ok\r\n")
-					else:
-						self.write(sock, status + "\r\n")
-			else:
-				if line and line[0] == ".":
-					self.__data.write(line[1:] + "\n")
-				else:
-					self.__data.write(line + "\n")
+                    if not status:
+                        self.write(sock, "250 Ok\r\n")
+                    else:
+                        self.write(sock, status + "\r\n")
+            else:
+                if line and line[0] == ".":
+                    self.__data.write(line[1:] + "\n")
+                else:
+                    self.__data.write(line + "\n")
 
-	###
-	### SMTP and ESMTP Commands
-	###
+    ###
+    ### SMTP and ESMTP Commands
+    ###
 
-	def smtpHELO(self, sock, arg):
-		if not arg:
-			self.write(sock, "501 Syntax: HELO hostname\r\n")
-			return
+    def smtpHELO(self, sock, arg):
+        if not arg:
+            self.write(sock, "501 Syntax: HELO hostname\r\n")
+            return
 
-		if self.__greeting:
-			self.write(sock, "503 Duplicate HELO/EHLO\r\n")
-		else:
-			self.__greeting = arg
-			self.write(sock, "250 %s\r\n" % self.__fqdn)
-			self.push(Helo(sock, arg), "helo", self.channel)
+        if self.__greeting:
+            self.write(sock, "503 Duplicate HELO/EHLO\r\n")
+        else:
+            self.__greeting = arg
+            self.write(sock, "250 %s\r\n" % self.__fqdn)
+            self.push(Helo(sock, arg), "helo", self.channel)
 
-	def smtpNOOP(self, sock, arg):
-		if arg:
-			self.write(sock, "501 Syntax: NOOP\r\n")
-		else:
-			self.write(sock, "250 Ok\r\n")
-			self.push(Noop(sock), "noop", self.channel)
+    def smtpNOOP(self, sock, arg):
+        if arg:
+            self.write(sock, "501 Syntax: NOOP\r\n")
+        else:
+            self.write(sock, "250 Ok\r\n")
+            self.push(Noop(sock), "noop", self.channel)
 
-	def smtpQUIT(self, sock, arg):
-		self.write(sock, "221 Bye\r\n")
-		self.close(sock)
-		self.push(Quit(sock), "quit", self.channel)
+    def smtpQUIT(self, sock, arg):
+        self.write(sock, "221 Bye\r\n")
+        self.close(sock)
+        self.push(Quit(sock), "quit", self.channel)
 
-	def smtpMAIL(self, sock, arg):
-		address = getAddress("FROM:", arg)
+    def smtpMAIL(self, sock, arg):
+        address = getAddress("FROM:", arg)
 
-		if not address:
-			self.write(sock, "501 Syntax: MAIL FROM:<address>\r\n")
-			return
+        if not address:
+            self.write(sock, "501 Syntax: MAIL FROM:<address>\r\n")
+            return
 
-		if self.__mailfrom:
-			self.write(sock, "503 Error: nested MAIL command\r\n")
-			return
+        if self.__mailfrom:
+            self.write(sock, "503 Error: nested MAIL command\r\n")
+            return
 
-		self.__mailfrom = address
+        self.__mailfrom = address
 
-		self.write(sock, "250 Ok\r\n")
-		self.push(Mail(sock, address), "mail", self.channel)
+        self.write(sock, "250 Ok\r\n")
+        self.push(Mail(sock, address), "mail", self.channel)
 
-	def smtpRCPT(self, sock, arg):
-		if not self.__mailfrom:
-			self.write(sock, "503 Error: need MAIL command\r\n")
-			return
+    def smtpRCPT(self, sock, arg):
+        if not self.__mailfrom:
+            self.write(sock, "503 Error: need MAIL command\r\n")
+            return
 
-		address = getAddress("TO:", arg)
+        address = getAddress("TO:", arg)
 
-		if not address:
-			self.write(sock, "501 Syntax: RCPT TO: <address>\r\n")
-			return
+        if not address:
+            self.write(sock, "501 Syntax: RCPT TO: <address>\r\n")
+            return
 
-		self.__rcpttos.append(address)
-		self.write(sock, "250 Ok\r\n")
-		self.push(Rcpt(sock, address), "rcpt", self.channel)
+        self.__rcpttos.append(address)
+        self.write(sock, "250 Ok\r\n")
+        self.push(Rcpt(sock, address), "rcpt", self.channel)
 
-	def smtpRSET(self, sock, arg):
-		if arg:
-			self.write(sock, "501 Syntax: RSET\r\n")
-			return
+    def smtpRSET(self, sock, arg):
+        if arg:
+            self.write(sock, "501 Syntax: RSET\r\n")
+            return
 
-		# Resets the sender, recipients, and data, but not the greeting
-		self.__mailfrom[sock] = None
-		self.__rcpttos = []
-		self.__data = None
-		self.__states = {}
-		self.write(sock, "250 Ok\r\n")
-		self.push(Rset(sock), "rset", self.channel)
+        # Resets the sender, recipients, and data, but not the greeting
+        self.__mailfrom[sock] = None
+        self.__rcpttos = []
+        self.__data = None
+        self.__states = {}
+        self.write(sock, "250 Ok\r\n")
+        self.push(Rset(sock), "rset", self.channel)
 
-	def smtpDATA(self, sock, arg):
-		if not self.__rcpttos:
-			self.write(sock, "503 Error: need RCPT command\r\n")
-			return
+    def smtpDATA(self, sock, arg):
+        if not self.__rcpttos:
+            self.write(sock, "503 Error: need RCPT command\r\n")
+            return
 
-		if arg:
-			self.write(sock, "501 Syntax: DATA\r\n")
-			return
+        if arg:
+            self.write(sock, "501 Syntax: DATA\r\n")
+            return
 
-		self.__state = self.DATA
-		self.__data = TemporaryFile()
+        self.__state = self.DATA
+        self.__data = TemporaryFile()
 
-		self.write(sock, "354 End data with <CR><LF>.<CR><LF>\r\n")
-		self.push(Data(sock), "data", self.channel)
+        self.write(sock, "354 End data with <CR><LF>.<CR><LF>\r\n")
+        self.push(Data(sock), "data", self.channel)
 
-	###
-	### Default Socket Events
-	###
+    ###
+    ### Default Socket Events
+    ###
 
-	@handler("connect")
-	def onCONNECT(self, sock, host, port):
-		self.__states[sock] = self.COMMAND
-		self.__buffers[sock] = ""
-		self.write(sock, "220 %s ???\r\n" % self.__fqdn)
+    @handler("connect")
+    def onCONNECT(self, sock, host, port):
+        self.__states[sock] = self.COMMAND
+        self.__buffers[sock] = ""
+        self.write(sock, "220 %s ???\r\n" % self.__fqdn)
 
-	@handler("disconnect")
-	def onDISCONNECT(self, sock):
-		self.reset()
+    @handler("disconnect")
+    def onDISCONNECT(self, sock):
+        self.reset()
