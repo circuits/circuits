92bcb1c9e12d67b1b1262e66d962300efbc62079
prologic <none@none>
2010-02-10 00:51:21 +1000

    Removed old tests directories

--- circuits/core/tests/__init__.py
+++
@@ -1,5 +0,0 @@
-# Package:  tests
-# Date:     4th August 2004
-# Author:   James Mills, prologic at shortcircuit dot net dot au
-
-"""core Teste Suite"""
--- circuits/web/tests/__init__.py
+++
@@ -1,5 +0,0 @@
-# Package:	tests
-# Date:		16th February 2009
-# Author:	James Mills, prologic at shortcircuit dot net dot au
-
-"""Web Test Suite"""
