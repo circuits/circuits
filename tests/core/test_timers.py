7346a7daabc14011f6f1e635f110422d9143f38f
prologic <none@none>
2010-02-10 14:07:05 +1000

    tests.core.test_timers: Added timers tests

---
+++ tests/core/test_timers.py
@@ -0,0 +1,39 @@
+# Module:   test_timers
+# Date:     10th February 2010
+# Author:   James Mills, prologic at shortcircuit dot net dot au
+
+"""Timers Tests"""
+
+from time import sleep
+
+from circuits import Event, Component, Timer
+
+class Test(Event):
+    """Test Event"""
+
+class App(Component):
+
+    flag = False
+
+    def timer(self):
+        self.flag = True
+
+app = App()
+app.start()
+
+def test_timer():
+    timer = Timer(0.1, Test(), "timer")
+    timer.register(app)
+    sleep(1)
+    assert app.flag
+
+def test_persistentTimer():
+    timer = Timer(0.1, Test(), "timer", persist=True)
+    timer.register(app)
+
+    for i in xrange(5):
+        sleep(1)
+        assert app.flag
+        app.flag = False
+
+    timer.unregister()
