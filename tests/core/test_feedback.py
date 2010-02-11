0ccf2968d9f49848ec15618660f0d26a94ba933b
prologic <none@none>
2010-02-11 18:34:22 +1000

    tests.core.test_feedback: Added tests for feature 'Event Feedback Channels' - not yet finished, but first test passes

---
+++ tests/core/test_feedback.py
@@ -0,0 +1,153 @@
+# Module:   test_feedback
+# Date:     11th February 2010
+# Author:   James Mills, prologic at shortcircuit dot net dot au
+
+"""Feedback Channels Tests"""
+
+import py
+
+from circuits import handler, Event, Component#, Debugger
+
+class Test(Event):
+    """Test Event"""
+
+    success = ("test_successful",)
+    failure = ("test_failed",)
+    filter = ("test_filtered",)
+    start = ("test_started",)
+    end = ("test_ended",)
+
+class App(Component):
+
+    def __init__(self):
+        super(App, self).__init__()
+
+        self.states = []
+        self.events = []
+        self.values = []
+        self.errors = []
+        self.handlers = []
+
+    @handler(filter=True)
+    def event(self, event, *args, **kwargs):
+        """Global Event Handler (filter)"""
+
+        if kwargs.get("filter", False):
+            return True
+
+    def test(self, error=False):
+        if error:
+            raise Exception("Hello World!")
+
+        return "Hello World!"
+
+    def test_successful(self, evt, handler, retval):
+        self.states.append("success")
+        self.events.append(evt)
+        self.handlers.append(handler)
+        self.values.append(retval)
+
+    def test_failed(self, evt, handler, error):
+        self.states.append("failure")
+        self.events.append(evt)
+        self.handlers.append(handler)
+        self.errors.append(retval)
+
+    def test_filtered(self, evt, handler, retval):
+        self.states.append("filter")
+        self.events.append(evt)
+        self.handlers.append(handler)
+        self.values.append(retval)
+
+    def test_started(self, evt):
+        self.states.append("start")
+        self.events.append(evt)
+
+    def test_ended(self, evt, handler, retval):
+        self.states.append("end")
+        self.events.append(evt)
+        self.handlers.append(handler)
+        self.values.append(retval)
+
+def reraise(e):
+    raise e
+
+def test():
+    app = App()# + Debugger()
+    while app:
+        app.flush()
+
+    e = Test()
+    x = app.push(e)
+
+    app.flush()
+
+    # "start" feedback
+    assert app.states[-1:][0] == "start"
+    assert app.events[-1:][0] == e
+    assert not app.values
+    assert not app.errors
+    assert not app.handlers
+
+    # The Event
+    s = x.value
+    assert s == "Hello World!"
+
+    app.flush()
+
+    # "success", "failure" or "filter" and "end" feedback
+    assert app.states[-2:][0] == "success"
+    assert app.events[-2:][0] == e
+    assert app.values[-2:][0] == "Hello World!"
+    assert not app.errors
+    assert app.handlers[-2:][0] == app.test
+
+    assert app.states[-1:][0] == "end"
+    assert app.events[-1:][0] == e
+    assert app.values[-1:][0] == "Hello World!"
+    assert not app.errors
+    assert app.handlers[-1:][0] == app.test
+
+@py.test.skip("TODO")
+def test_failure():
+    app = App()# + Debugger()
+    while app:
+        app.flush()
+
+    e = Test(error=True)
+    x = app.push(e)
+
+    app.flush()
+
+    # "start" feedback
+    assert app.states[-1:][0] == "start"
+    assert app.events[-1:][0] == e
+    assert not app.values
+    assert not app.errors
+    assert not app.handlers
+
+    # The Event
+    py.test.raises(Exception, lambda x: reraise(x[1]), x.value)
+
+    app.flush()
+
+    # "success", "failure" or "filter" and "end" feedback
+    assert app.states[-2:][0] == "failure"
+    assert app.events[-2:][0] == e
+    py.test.raises(Exception, lambda x: reraise(x[1]), app.values[-2:][0])
+    etype, evalue, etraceback = app.errors[-2:][0]
+    assert etype is Exception
+    assert type(evalue) is Exception
+    assert isinstance(evalue, Excpetion)
+    assert type(etraceback) is traceback
+    assert app.handlers[-2:][0] == app.test
+
+    assert app.states[-1:][0] == "end"
+    assert app.events[-1:][0] == e
+    py.test.raises(Exception, lambda x: reraise(x[1]), app.values[-1:][0])
+    etype, evalue, etraceback = app.errors[-1:][0]
+    assert etype is Exception
+    assert type(evalue) is Exception
+    assert isinstance(evalue, Excpetion)
+    assert type(etraceback) is traceback
+    assert app.handlers[-1:][0] == app.test
