e65ae41acf3c112a210b1259ea3751453947f0b1
prologic <none@none>
2010-03-08 01:47:04 +1000

    tests.web.test_wsgi_application: Added tests (copy of test_core) that tests against a wsgi deployed app

---
+++ tests/web/test_wsgi_application.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+
+from urllib import urlencode
+from urllib2 import urlopen, HTTPError
+
+from circuits.tools import find
+from circuits.web import Controller, Request
+from circuits.web.wsgi import Application
+
+class Root(Controller):
+
+    def index(self):
+        return "Hello World!"
+
+    def test_args(self, *args, **kwargs):
+        return "%s\n%s" % (repr(args), repr(kwargs))
+
+    def test_redirect(self):
+        return self.redirect("/")
+
+    def test_forbidden(self):
+        return self.forbidden()
+
+    def test_notfound(self):
+        return self.notfound()
+
+    def test_event(self, event, *args, **kwargs):
+        self.event = event
+        return "%s\n%s" % (repr(args), repr(kwargs))
+
+application = Application() + Root()
+
+def test(webapp):
+    f = urlopen(webapp.server.base)
+    s = f.read()
+    assert s == "Hello World!"
+
+def test_404(webapp):
+    try:
+        urlopen("%s/foo" % webapp.server.base)
+    except HTTPError, e:
+        assert e.code == 404
+        assert e.msg == "Not Found"
+    else:
+        assert False
+
+def test_args(webapp):
+    args = ("1", "2", "3")
+    kwargs = {"1": "one", "2": "two", "3": "three"}
+    url = "%s/test_args/%s" % (webapp.server.base, "/".join(args))
+    data = urlencode(kwargs)
+
+    f = urlopen(url, data)
+    data = f.read().split("\n")
+    assert data[0] == repr(args)
+    assert data[1] == repr(kwargs)
+
+def test_redirect(webapp):
+    f = urlopen("%s/test_redirect" % webapp.server.base)
+    s = f.read()
+    assert s == "Hello World!"
+
+def test_forbidden(webapp):
+    try:
+        urlopen("%s/test_forbidden" % webapp.server.base)
+    except HTTPError, e:
+        assert e.code == 403
+        assert e.msg == "Forbidden"
+    else:
+        assert False
+
+def test_notfound(webapp):
+    try:
+         urlopen("%s/test_notfound" % webapp.server.base)
+    except HTTPError, e:
+        assert e.code == 404
+        assert e.msg == "Not Found"
+    else:
+        assert False
