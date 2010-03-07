b3eb1ef21635423405281cc0438124942bb7a687
prologic <none@none>
2010-03-08 01:45:41 +1000

    test.web.test_wsgi_gateway: Our conftest.py for py.test automatically picks up application (wsgi) objects and registers them with a circuits.web.wsgi.Gateway

--- tests/web/test_wsgi_gateway.py
+++ tests/web/test_wsgi_gateway.py
@@ -2,20 +2,13 @@
 
 from urllib2 import urlopen
 
-from circuits.web.wsgi import Application, Gateway
-
-def app(environ, start_response):
+def application(environ, start_response):
     status = "200 OK"
     response_headers = [("Content-type", "text/plain")]
     start_response(status, response_headers)
     return "Hello World!"
 
 def test(webapp):
-    gateway = Gateway(app)
-    gateway.register(webapp)
-
     f = urlopen(webapp.server.base)
     s = f.read()
     assert s == "Hello World!"
-
-    gateway.unregister()
