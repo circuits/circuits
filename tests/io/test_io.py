30a69ccead96667f5e2a953ce397652d60be3498
prologic <none@none>
2010-03-23 20:59:46 +1000

    tests.io: Added io tests

---
+++ tests/io/test_io.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+
+from circuits import Component
+from circuits.io import File, Write
+
+class App(Component):
+
+    def __init__(self, filename, mode):
+        super(App, self).__init__()
+
+        self._file = File(filename, mode)
+        self._file.register(self)
+
+        self.data = None
+        self.eof = False
+
+    def read(self, data):
+        self.data = data
+
+    def eof(self):
+        self.eof = True
+
+def test_write(tmpdir):
+    sockpath = tmpdir.ensure("helloworld.txt")
+    filename = str(sockpath)
+
+    app = App(filename, "w")
+    app.start()
+    app.push(Write("Hello World!"))
+    app.stop()
+
+    f = open(filename, "r")
+    s = f.read()
+    assert s == "Hello World!"
+
+def test_read(tmpdir):
+    sockpath = tmpdir.ensure("helloworld.txt")
+    filename = str(sockpath)
+
+    f = open(filename, "w")
+    f.write("Hello World!")
+    f.close()
+
+    app = App(filename, "r")
+    app.start()
+
+    while not app.eof:
+        pass
+
+    app.stop()
+
+    assert app.data == "Hello World!"
