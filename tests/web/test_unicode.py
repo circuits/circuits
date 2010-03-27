6d30c67164432849c8a82a3f82bbb855b28e64c7
prologic <none@none>
2010-03-27 23:41:10 +1000

    tests.web: Added test_unicode testing basic unicode and unicode url support in circuits.web

---
+++ tests/web/test_unicode.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from urllib2 import urlopen
+
+from circuits.web.utils import url_quote
+from circuits.web import expose, Controller
+
+class Root(Controller):
+
+    def index(self):
+        return "Hello World!"
+
+    @expose(url_quote(u"你好"))
+    def hello(self):
+        self.response.headers["Content-Type"] = "text/html; charset=utf-8"
+        return u"世界您好"
+
+def test(webapp):
+    f = urlopen(webapp.server.base)
+    s = f.read()
+    assert s == "Hello World!"
+
+    url = u"%s/你好" % webapp.server.base
+    f = urlopen(url_quote(url.encode("utf-8")))
+    s = f.read()
+    s = s.decode("utf-8")
+    assert s == u"世界您好"
