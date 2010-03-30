a52c1c9f66c4934cc8fbf260f96b0252c09db7cb
prologic <none@none>
2010-03-31 04:14:17 +1000

    tests.core.test_future: Added test_error() testing possible exceptions encountered while processing a future value.

--- tests/core/test_future.py
+++ tests/core/test_future.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python -i
 
+from types import TracebackType
+
+import py
+
 from circuits import future, Event, Component
 
 class Hello(Event):
-    "Hello Event"
+    """Hello Event"""
 
-class Future(Event):
-    "Future Event"
+class Test(Event):
+    """Test Event"""
+
+class Error(Event):
+    """Error Event"""
 
 class App(Component):
 
@@ -14,14 +21,37 @@ class App(Component):
         return "Hello World!"
 
     @future()
-    def future(self):
+    def test(self):
         return self.push(Hello())
 
-def test_future_value():
+    @future()
+    def error(self):
+        raise Exception("Hello World!")
+
+def reraise(e):
+    raise e
+
+def test():
     app = App()
     while app:
         app.flush()
-    x = app.push(Future())
+    x = app.push(Test())
     app.flush()
     app.flush()
     assert x.value == "Hello World!"
+
+def test_error():
+    app = App()
+    while app:
+        app.flush()
+    x = app.push(Error())
+    app.flush()
+    app.flush()
+    print repr(x)
+    print repr(x._value)
+    print repr(x._value._value)
+    assert x.errors
+    etype, evalue, etraceback = x.value
+    assert etype is Exception
+    py.test.raises(Exception, lambda e: reraise(e), evalue)
+    assert type(etraceback) is TracebackType
