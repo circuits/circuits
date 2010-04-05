de23c10a39ed886939967dcb4fc06e504193abf7
prologic <none@none>
2010-04-06 05:50:48 +1000

    setup.py refactored - attributes moved out of circuits/__init__.py -- long description put in top-level README

--- circuits/__init__.py
+++ circuits/__init__.py
@@ -2,98 +2,20 @@
 # Date:		3rd October 2008
 # Author:	James Mills, prologic at shortcircuit dot net dot au
 
-"""A Lightweight, Event driven Framework with a strong Component Architecture.
+"""
+    circuits
+    ~~~~~~~~
 
-Components communicate with one another by propagating Events on Channels
-throughout the System. Each Component has a set of Event Handlers that
-can listen for or filter Events on one or more Channels. Components react
-to Events and in turn expose further Events into the System. Each Component
-is capable of managing it's own Events as well as those of other Components.
-Complex directed graph structures can be created with
-Component Registrations, this gives a level of hierarchy and separation
-of concern.
+    Lightweight Event driven Framework
 
-Example:
-   >>> from time import sleep
-   >>> from circuits import Event, Component
-   >>>
-   >>> class App(Component):
-   ...   def hello(self):
-   ...      print "Hello World!"
-   >>> app = App()
-   >>> app.start()
-   >>> app.push(Event(), "hello")
-   >>> sleep(1)
-   Hello World!
+    :copyright: CopyRight (C) 2004-2010 by James Mills
+    :license: MIT (See: LICENSE)
 """
 
 try:
     from __version__ import version as __version__
 except ImportError:
     __version__ = "unknown"
-
-__name__ = "circuits"
-__description__ = "Event framework with a Component architecture"
-__author__ = "James Mills"
-__author_email__ = "%s, prologic at shortcircuit dot net dot au" % __author__
-__maintainer__ = __author__
-__maintainer_email__ = __author_email__
-__url__ = "http://code.google.com/p/circuits/"
-__download_url__ = "http://circuits.googlecode.com/files/%s-%s.tar.gz" % (__name__, __version__)
-__copyright__ = "Copyright (C) 2004-2009 by %s" % __author__
-__license__ = "MIT"
-__platforms__ = "POSIX"
-__keywords__ = "circuits Event Library Framework Component Architecture"
-__classifiers__ = [
-	"Development Status :: 4 - Beta",
-	"Environment :: Console",
-	"Environment :: No Input/Output (Daemon)",
-	"Environment :: Other Environment",
-	"Environment :: Plugins",
-	"Environment :: Web Environment",
-	"Intended Audience :: Developers",
-	"Intended Audience :: Information Technology",
-	"Intended Audience :: Science/Research",
-	"Intended Audience :: System Administrators",
-	"Intended Audience :: Telecommunications Industry",
-	"License :: OSI Approved",
-	"License :: OSI Approved :: MIT License",
-	"Natural Language :: English",
-	"Operating System :: POSIX",
-	"Operating System :: POSIX :: Linux",
-	"Programming Language :: Python",
-	"Programming Language :: Python :: 2.5",
-	"Programming Language :: Python :: 2.6",
-	"Programming Language :: Python :: 3.0",
-	"Topic :: Adaptive Technologies",
-	"Topic :: Communications :: Chat",
-	"Topic :: Communications :: Chat :: Internet Relay Chat",
-	"Topic :: Communications :: Email",
-	"Topic :: Communications :: Email :: Mail Transport Agents",
-	"Topic :: Database",
-	"Topic :: Internet",
-	"Topic :: Internet :: WWW/HTTP",
-	"Topic :: Internet :: WWW/HTTP :: HTTP Servers",
-	"Topic :: Internet :: WWW/HTTP :: WSGI",
-	"Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
-	"Topic :: Internet :: WWW/HTTP :: WSGI :: Middleware",
-	"Topic :: Internet :: WWW/HTTP :: WSGI :: Server",
-	"Topic :: Software Development :: Libraries",
-	"Topic :: Software Development :: Libraries :: Application Frameworks",
-	"Topic :: Software Development :: Libraries :: Python Modules",
-	"Topic :: System :: Clustering",
-	"Topic :: System :: Distributed Computing"]
-__str__ = "%s-%s" % (__name__, __version__)
-__package_data__ = {}
-__install_requires__ = []
-__setup_requires__ = []
-__extras_require__ = {}
-__entry_points__ = """\
-[console_scripts]
-circuits.bench = circuits.tools.bench:main
-circuits.sniffer = circuits.tools.sniffer:main
-circuits.web = circuits.web.main:main
-"""
 
 from core import handler, Event, Component,  Manager
 
--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 
-
 try:
     from setuptools import setup, find_packages
     HAS_SETUPTOOLS = True
@@ -10,52 +9,82 @@ except ImportError:
 
 if not HAS_SETUPTOOLS:
     import os
+    from os import getcwdd, listdir
+    from os.path import isdir, isfile
 
     def is_package(path):
-        return (
-            os.path.isdir(path) and
-            os.path.isfile(os.path.join(path, '__init__.py')
-            ))
+        return isdir(path) and isfile(os.path.join(path, "__init__.py"))
 
-    def find_packages(path, base=""):
-        """ Find all packages in path """
+    def find_packages(where="."):
         packages = {}
-        for item in os.listdir(path):
-            dir = os.path.join(path, item)
+        for item in listdir(where):
+            dir = os.path.join(where, item)
             if is_package( dir ):
-                if base:
-                    module_name = "%(base)s.%(item)s" % vars()
-                else:
-                    module_name = item
+                module_name = item
                 packages[module_name] = dir
                 packages.update(find_packages(dir, module_name))
         return packages
 
-from circuits import version
-version.forget_version()
-version.remember_version()
+from circuits.version import forget_version, remember_version
 
-import circuits as pkg
+forget_version()
+version = remember_version()
 
 setup(
-		name=pkg.__name__,
-		version=pkg.__version__,
-		description=pkg.__description__,
-		long_description=pkg.__doc__,
-		author=pkg.__author__,
-		author_email=pkg.__author_email__,
-		maintainer=pkg.__maintainer__,
-		maintainer_email=pkg.__maintainer_email__,
-		url=pkg.__url__,
-		download_url=pkg.__download_url__,
-		classifiers=pkg.__classifiers__,
-		license=pkg.__license__,
-		keywords=pkg.__keywords__,
-		platforms=pkg.__platforms__,
-		packages=find_packages("."),
-		install_requires=pkg.__install_requires__,
-		setup_requires=pkg.__setup_requires__,
-		extras_require=pkg.__extras_require__,
-		entry_points=pkg.__entry_points__,
-		package_data=pkg.__package_data__,
+    name="circuits",
+    version=version,
+    description="Lightweight Event driven Framework",
+    long_description=open("README", "r").read(),
+    author="James Mills",
+    author_email="James Mills, prologic at shortcircuit dot net dot au",
+    url="http://bitbucket.org/prologic/circuits/",
+    download_url="http://bitbucket.org/prologic/circuits/downloads/",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Environment :: No Input/Output (Daemon)",
+        "Environment :: Other Environment",
+        "Environment :: Plugins",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: System Administrators",
+        "Intended Audience :: Telecommunications Industry",
+        "License :: OSI Approved",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: POSIX",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.5",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 3.0",
+        "Topic :: Adaptive Technologies",
+        "Topic :: Communications :: Chat",
+        "Topic :: Communications :: Chat :: Internet Relay Chat",
+        "Topic :: Communications :: Email",
+        "Topic :: Communications :: Email :: Mail Transport Agents",
+        "Topic :: Database",
+        "Topic :: Internet",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: HTTP Servers",
+        "Topic :: Internet :: WWW/HTTP :: WSGI",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Middleware",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Server",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Application Frameworks",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: System :: Clustering",
+        "Topic :: System :: Distributed Computing"],
+    license="MIT",
+    keywords="event framework distributed concurrent component asynchronous",
+    platforms="POSIX",
+    packages=find_packages("."),
+    entry_points="""
+    [console_scripts]
+    circuits.bench = circuits.tools.bench:main
+    circuits.web = circuits.web.main:main
+    """
 )
