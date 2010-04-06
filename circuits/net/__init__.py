39bb51d45fce04bd889416ccc2c11e84b07817cb
prologic <none@none>
2010-04-07 06:35:28 +1000

    Have a guess :)

---
+++ circuits/net/__init__.py
@@ -0,0 +1,11 @@
+# Package:  net
+# Date:     7th April 2010
+# Author:   James Mills, prologic at shortcircuit dot net dot au
+
+"""
+    Networking Components
+    ~~~~~~~~~~~~~~~~~~~~~
+
+    This package contains components that implement network sockets and
+    pollers for implementing client and server network applications.
+"""
