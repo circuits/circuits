5183bcb8c6b5b70fa6dc611bbd38c452cec16031
prologic <none@none>
2010-04-07 03:22:33 +1000

    Docs docs docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,13 +22,10 @@ import sys, os
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.coverage', 'sphinx.ext.todo', 'sphinx.ext.autosummary']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.autosummary']
 
 # autosummary_generate  list
-autosummary_generate = ["api"]
-
-# Include todo items
-todo_include_todos = True
+autosummary_generate = True
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
