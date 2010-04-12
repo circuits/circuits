1cefc26ad0d992971e99465c31f5569c8ea74045
prologic <none@none>
2010-04-12 12:23:38 +1000

    tests.app.app: Fixed coverage output for app for tests.app.test_app(...)

--- tests/app/app.py
+++ tests/app/app.py
@@ -16,7 +16,7 @@ class App(Component):
         Daemon(pidfile, stderr="/tmp/app.log").register(self)
 
 def main():
-    _coverage = coverage()
+    _coverage = coverage(data_suffix=True)
     _coverage.start()
 
     pidfile = os.path.abspath(sys.argv[1])
