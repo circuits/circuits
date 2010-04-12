33c33b792dc1ed9acdd3f5331afd5a42385d20ce
prologic <none@none>
2010-04-12 10:26:55 +1000

    examples: Use Write Event in echoserver.py

--- examples/echoserver.py
+++ examples/echoserver.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 
-from circuits.net.sockets import TCPServer
+from circuits.net.sockets import TCPServer, Write
 
 class EchoServer(TCPServer):
 
     def read(self, sock, data):
-        self.write(sock, data)
+        self.push(Write(sock, data))
     
 EchoServer(8000).run()
