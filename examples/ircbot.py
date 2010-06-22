123df36adec6e27ea20daf998fe8c0094ace78c0
prologic <none@none>
2010-06-23 00:44:18 +1000

    examples/ircbot.py: Make this example join #circuits only and do so on RPL_ENDOFMOTD or ERR_NOMOTD rather than at connection time. Closes issue #8

--- examples/ircbot.py
+++ examples/ircbot.py
@@ -4,25 +4,28 @@ from circuits import Component, Debugger
 from circuits.net.sockets import TCPClient, Connect
 from circuits.net.protocols.irc import IRC, PRIVMSG, USER, NICK, JOIN
 
+from circuits.net.protocols.irc import ERR_NICKNAMEINUSE
+from circuits.net.protocols.irc import RPL_ENDOFMOTD, ERR_NOMOTD
+
 class Bot(Component):
 
-    def __init__(self, host, port=6667, channel=None):
-        super(Bot, self).__init__(channel=channel)
-        self += TCPClient(channel=channel) + IRC(channel=channel)
+    def __init__(self, host, port=6667):
+        super(Bot, self).__init__()
+        self += TCPClient() + IRC()
         self.push(Connect(host, port))
 
     def connected(self, host, port):
         self.push(USER("circuits", host, host, "Test circuits IRC Bot"))
         self.push(NICK("circuits"))
-        self.push(JOIN("#shortcircuit"))
-        self.push(JOIN("#softcircuit"))
 
     def numeric(self, source, target, numeric, args, message):
-        if numeric == 433:
+        if numeric == ERR_NICKNAMEINUSE:
             self.push(NICK("%s_" % args))
+        if numeric in (RPL_ENDOFMOTD, ERR_NOMOTD):
+            self.push(JOIN("#circuits"))
 
     def message(self, source, target, message):
         self.push(PRIVMSG(source[0], message))
 
-bot = Bot("irc.freenode.net", channel="bot") + Debugger()
+bot = Bot("irc.freenode.net") + Debugger()
 bot.run()
