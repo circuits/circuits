8dccce77f6c08a7c20f38b9f1bacc27b71ab56a1
prologic <none@none>
2010-07-01 08:34:26 +1000

    examples/web/wiki: Change the output of <<macros>>

--- examples/web/wiki/macros/utils.py
+++ examples/web/wiki/macros/utils.py
@@ -3,9 +3,12 @@
 Utility macros
 """
 
+from inspect import getdoc
+
 def macros(macro, environ, *args, **kwargs):
     """Return a list of available macros"""
 
-    s = "\n".join(["* %s" % k for k in environ["macros"].keys()])
+    macros = environ["macros"].items()
+    s = "\n".join(["== %s ==\n%s\n" % (k, getdoc(v)) for k, v in macros])
 
     return environ["parser"].generate(s, environ=environ)
