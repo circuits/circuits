c5e9c23c6a502dd117314efd17a91478355bf77a
prologic <none@none>
2010-07-24 23:12:18 +1000

    Added tests for testing @future against BaseComponent(s)

--- tests/core/test_future.py
+++ tests/core/test_future.py
@@ -4,7 +4,7 @@ from types import ListType
 
 import py
 
-from circuits import future, Event, Component
+from circuits import future, handler, BaseComponent, Component, Event
 
 class Hello(Event):
     """Hello Event"""
@@ -28,6 +28,22 @@ class App(Component):
     def error(self):
         raise Exception("Hello World!")
 
+class BaseApp(BaseComponent):
+
+    @handler("hello")
+    def _on_hello(self):
+        return "Hello World!"
+
+    @handler("test")
+    @future()
+    def _on_test(self):
+        return self.push(Hello())
+
+    @handler("error")
+    @future()
+    def _on_error(self):
+        raise Exception("Hello World!")
+
 def reraise(e):
     raise e
 
@@ -44,6 +60,32 @@ def test():
 
 def test_error():
     app = App()
+    while app: app.flush()
+    e = Error()
+    assert e.future == False
+    x = app.push(e)
+    while not x.errors:
+        app.flush()
+    assert e.future == True
+    assert x.errors
+    etype, evalue, etraceback = x.value
+    assert etype is Exception
+    py.test.raises(Exception, lambda e: reraise(e), evalue)
+    assert type(etraceback) is ListType
+
+def test_base():
+    app = BaseApp()
+    while app: app.flush()
+    e = Test()
+    assert e.future == False
+    x = app.push(e)
+    while not x.result:
+        app.flush()
+    assert e.future == True
+    assert x.value == "Hello World!"
+
+def test_base_error():
+    app = BaseApp()
     while app: app.flush()
     e = Error()
     assert e.future == False
