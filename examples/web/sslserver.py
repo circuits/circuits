c35a704652c58e16da4e83f8ab20cf6aa3ee81a2
Dariusz Suchojad <dsuch@gefira.pl>
2010-10-07 17:31:14 +0200

    Reverted to the previous version.

--- examples/web/sslserver.py
+++ examples/web/sslserver.py
@@ -1,10 +1,5 @@
 #!/usr/bin/env python
 
-# stdlib
-import ssl
-
-# Circuits
-from circuits import Debugger
 from circuits.web import Server, Controller
 
 class Root(Controller):
@@ -12,5 +7,4 @@ class Root(Controller):
     def index(self):
         return "Hello World!"
 
-(Server(("localhost", 8443), ssl=True, certfile="server-cert.pem", keyfile="server-key.pem",
-        ca_certs="ca-chain.pem", cert_reqs=ssl.CERT_NONE) + Debugger() + Root()).run()
+(Server(8000, ssl=True, certfile="cert.pem") + Root()).run()
