500a554e30a88888744f60f97c39e519869fb62a
prologic <none@none>
2011-02-01 10:19:25 +1000

    New release

--- circuits/__init__.py
+++ circuits/__init__.py
@@ -12,7 +12,7 @@ Lightweight Event driven Framework
 """
 
 __author__ = "James Mills"
-__date__ = "25 January 2011"
-__version__ = "1.3"
+__date__ = "1st February 2011"
+__version__ = "1.3.2"
 
 from core import *
