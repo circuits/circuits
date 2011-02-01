383d07723fe967761bcdfc112703703b911f3b20
prologic <none@none>
2011-02-01 10:20:38 +1000

    Fixed controllers circuits.web example

--- examples/web/controllers.py
+++ examples/web/controllers.py
@@ -5,10 +5,6 @@ from circuits.web import Server, Controller, Logger
 class Root(Controller):
 
     def index(self):
-        agent = self.request.headers["User-Agent"]
-        if "iPad" in agent:
-            return "Hello iPad!"
-        else:
-            return "Hello World!"
+        return "Hello World!"
 
 (Server(("0.0.0.0", 8000)) + Root() + Logger()).run()
