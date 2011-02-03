89ffcc0120e2edbb9080ea4f922e35587603f182
prologic <none@none>
2011-02-04 09:00:26 +1000

    examples/web/filtering.py: Fixed example

--- examples/web/filtering.py
+++ examples/web/filtering.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python
 
-from circuits import Component
+from circuits import handler, Component
 from circuits.web import Server, Controller
 
 class Upper(Component):
 
-    channel = "http"
+    channel = "web"
 
-    def response(self, response):
-        response.body = response.body.upper()
+    @handler("response", priority=1.0)
+    def _on_response(self, response):
+        response.body = "".join(response.body).upper()
 
 class Root(Controller):
 
