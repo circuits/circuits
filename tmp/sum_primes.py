9bfbcf6f2e2039628a37f042f636add6648762c7
prologic <none@none>
2011-02-11 21:59:44 +1000

    tmp/sum_primes.py: For testing of concurrency and distributed computing with circuits

---
+++ tmp/sum_primes.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import sys
+import math
+
+from circuits import Component, Pool, Task
+
+def isprime(n):
+    """Returns True if n is prime and False otherwise"""
+    if not isinstance(n, int):
+        raise TypeError("argument passed to is_prime is not of 'int' type")
+    if n < 2:
+        return False
+    if n == 2:
+        return True
+    max = int(math.ceil(math.sqrt(n)))
+    i = 2
+    while i <= max:
+        if n % i == 0:
+            return False
+        i += 1
+    return True
+
+def sum_primes(n):
+    """Calculates sum of all primes below given integer n"""
+    return sum([x for x in xrange(2, n) if isprime(x)])
+
+x = sum_primes(100)
