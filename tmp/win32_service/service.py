f8c8b2a2820a723f04b72b9c76e6dfd7e7e81c5f
jamesmills <none@none>
2011-02-11 16:37:14 +1000

    tmp/win32_service: Trying to implement our own Service Component instead of using win32serviceutil.ServiceFramework

--- tmp/win32_service/service.py
+++ tmp/win32_service/service.py
@@ -5,8 +5,8 @@ from sys import modules
 from traceback import format_exc
 from os.path import splitext, abspath
 
-from servicemanager import LogInfoMsg
 from win32api import SetConsoleCtrlHandler
+from servicemanager import LogInfoMsg, RegisterServiceCtrlHandler
 
 from win32serviceutil import InstallService, RemoveService
 from win32serviceutil import ServiceFramework, StartService
@@ -14,39 +14,80 @@ from win32serviceutil import ServiceFramework, StartService
 from win32event import INFINITE
 from win32event import CreateEvent, SetEvent, WaitForSingleObject
 
+from win32service import SetServiceStatus
 from win32service import SERVICE_STOPPED
+from win32service import SERVICE_CONTROL_STOP:
+from win32service import SERVICE_CONTROL_SHUTDOWN:
+from win32service import SERVICE_WIN32_OWN_PROCESS
+from win32service import SERVICE_CONTROL_INTERROGATE:
 from win32service import SERVICE_AUTO_START, SERVICE_RUNNING
 from win32service import SERVICE_START_PENDING, SERVICE_STOP_PENDING
+from win32service import SERVICE_ACCEPT_STOP, SERVICE_ACCEPT_SHUTDOWN
 
 from circuits import BaseComponent
 
-class Service(BaseComponent, ServiceFramework):
+class ServiceFramework:
 
     _svc_name_ = "unknown"
     _svc_display_name_ = "Service Template"
 
-    def __init__(self, *args):
-        BaseComponent.__init__(self)
-        ServiceFramework.__init__(self, *args)
+    channel = "service"
 
-        self._stop_event = CreateEvent(None, 0, 0, None)
+    def __init__(self, *args, channel=channel):
+        super(Service, self).__init__(channel=channel)
 
-    def SvcDoRun(self):
-        self.ReportServiceStatus(SERVICE_START_PENDING)
-        try:
-            self.ReportServiceStatus(SERVICE_RUNNING)
-            self.run()
-            WaitForSingleObject(self._stop_event, INFINITE)
-        except Exception, e:
-            LogInfoMsg("ERROR: %s" % e)
-            LogInfoMsg(format_exc())
+        self._ssh = RegisterServiceCtrlHandler(args[0], self.ServiceCtrlHandler)
+        self._checkPoint = 0
+
+    def GetAcceptedControls(self):
+        return SERVICE_ACCEPT_STOP | SERVICE_ACCEPT_SHUTDOWN
+
+    def ReportServiceStatus(self, serviceStatus, waitHint=5000, win32ExitCode=0,
+            svcExitCode = 0):
+        if self._ssh is None: # Debugging!
+            return
+        if serviceStatus == SERVICE_START_PENDING:
+            accepted = 0
+        else:
+            accepted = self.GetAcceptedControls()
+
+        if serviceStatus in [SERVICE_RUNNING,  SERVICE_STOPPED]:
+            checkPoint = 0
+        else:
+            self._checkPoint += 1
+            checkPoint = self._checkPoint
+
+        # Now report the status to the control manager
+        status = (SERVICE_WIN32_OWN_PROCESS,
+                 serviceStatus,
+                 accepted, # dwControlsAccepted,
+                 win32ExitCode, # dwWin32ExitCode;
+                 svcExitCode, # dwServiceSpecificExitCode;
+                 checkPoint, # dwCheckPoint;
+                 waitHint)
+        SetServiceStatus(self._ssh, status)
+
+    def SvcInterrogate(self):
+        # Assume we are running, and everyone is happy.
+        self.ReportServiceStatus(SERVICE_RUNNING)
+
+    def SvcOther(self, control):
+        print "Unknown control status - %d" % control
+
+    def ServiceCtrlHandler(self, control):
+        if control == SERVICE_CONTROL_STOP:
             self.SvcStop()
+        elif control == SERVICE_CONTROL_INTERROGATE:
+            self.SvcInterrogate()
+        elif control == SERVICE_CONTROL_SHUTDOWN:
+            self.SvcShutdown()
+        else:
+            self.SvcOther(control)
 
-    def SvcStop(self):
+    def SvcRun(self):
+        self.ReportServiceStatus(SERVICE_RUNNING)
+        self.run()
         self.ReportServiceStatus(SERVICE_STOP_PENDING)
-        self.stop()
-        SetEvent(self._stop_event)
-        self.ReportServiceStatus(SERVICE_STOPPED)
 
 def install_service(cls, name, description=None, stay_alive=True):
     cls._svc_name_ = name
