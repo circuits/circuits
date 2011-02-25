c67a15d0c099ba7351306209c83b76bd7cf14dc7
jamesmills <none@none>
2011-02-25 12:12:02 +1000

    - tmp/websockets: Working implementation of WebSockets with circuits.web !!!

--- tmp/websockets/server.py
+++ tmp/websockets/server.py
@@ -1,47 +1,32 @@
 #!/usr/bin/env python
 
-import collections
-import select
-import string
-import struct
-from errno import EAGAIN
-try:
-    from hashlib import md5
-except ImportError: #pragma NO COVER
-    from md5 import md5
-from errno import EINTR
-from socket import error as SocketError
+from struct import pack
+from hashlib import md5
+from collections import defaultdict
 
-from circuits.core.utils import findcmp
-from circuits.core.pollers import _Poller
 from circuits.web import Logger, Server, Static
 from circuits import handler, BaseComponent, Event
 
 from circuits import Debugger
 
-def _extract_number(value):
-    out = ""
-    spaces = 0
-    for char in value:
-        if char in string.digits:
-            out += char
-        elif char == " ":
-            spaces += 1
-    return int(out) / spaces
 
 class MalformedWebSocket(ValueError):
     """Malformed WebSocket Error"""
 
+
 class WebSocketEvent(Event):
     """WebSocket Event"""
+
 
 class WebSocketRequest(WebSocketEvent):
     """WebSocket Request Event"""
 
     channel = "websocket_request"
 
+
 class Message(WebSocketEvent):
     """Message Event"""
+
 
 class WebSockets(BaseComponent):
 
@@ -53,7 +38,7 @@ class WebSockets(BaseComponent):
         self.path = path
 
         self._clients = []
-        self._buffers = {}
+        self._buffers = defaultdict(str)
 
     def _parse_messages(self, sock):
         msgs = []
@@ -64,13 +49,14 @@ class WebSockets(BaseComponent):
             if frame_type == 0:
                 # Normal message.
                 end_idx = buf.find("\xFF")
-                if end_idx == -1: #pragma NO COVER
+                if end_idx == -1:  # pragma NO COVER
                     break
                 msgs.append(buf[1:end_idx].decode("utf-8", "replace"))
-                buf = buf[end_idx+1:]
+                buf = buf[(end_idx + 1):]
             elif frame_type == 255:
                 # Closing handshake.
-                assert ord(buf[1]) == 0, "Unexpected closing handshake: %r" % buf
+                assert ord(buf[1]) == 0, \
+                        "Unexpected closing handshake: %r" % buf
                 self.closed = True
                 break
             else:
@@ -81,7 +67,7 @@ class WebSockets(BaseComponent):
         return msgs
 
     @handler("read", filter=True)
-    def _on_read(self, sock):
+    def _on_read(self, sock, data):
         if sock in self._clients:
             self._buffers[sock] += data
             messages = self._parse_messages(sock)
@@ -89,73 +75,57 @@ class WebSockets(BaseComponent):
                 self.push(Message(sock, message))
             return True
 
-    @handler("websocket_request")
-    def _on_websocket_request(self, request, response):
-        headers = request.headers
-
-        if headers.get("Connection", None) == "Upgrade" and \
-            headers.get("Upgrade", None) == "WebSocket":
-
-            # See if they sent the new-format headers
-            if "Sec_WebSocket-Key1" in headers:
-                protocol_version = 76
-                if "Sec-WebSocket-Key2" not in headers:
-                    raise MalformedWebSocket()
-            else:
-                protocol_version = 75
-
-            # If it"s new-version, we need to work out our challenge response
-            if protocol_version == 76:
-                key1 = _extract_number(headers["Sec-WebSocket-Key1"])
-                key2 = _extract_number(headers["Sec-WebSocket-Key2"])
-                # There"s no content-length header in the request, but it has 8
-                # bytes of data.
-                key3 = request.body.read()
-                key = struct.pack(">II", key1, key2) + key3
-                handshake_response = md5(key).digest()
-
-            location = "ws://%s:%s%s" % (request.host,
-                    request.local.port, request.path)
-            qs = request.qs
-            if qs:
-                location += "?" + qs
-
-            if protocol_version == 75:
-                response.status = (101, "Web Socket Protocol Handshake")
-                response.headers["Upgrade"] =  "WebSocket"
-                response.headers["Connection"] =  "Upgrade"
-                response.headers["WebSocket-Origin"] = headers["Origin"]
-                response.headers["WebSocket-Location"] =  location
-            elif protocol_version == 76:
-                response.status = (101, "Web Socket Protocol Handshake")
-                response.headers["Upgrade"] =  "WebSocket"
-                response.headers["Connection"] =  "Upgrade"
-
-                response.headers["Sec-WebSocket-Origin"] = headers["Origin"]
-
-                response.headers["Sec-WebSocket-Protocol"] = \
-                        headers.get("Sec-WebSocket-Protocol", "default")
-                        
-                response.headers["Sec-WebSocket-Location"] = location
-            else:
-                raise MalformedWebSocket("Unknown WebSocket protocol version.")
-
-            socket = request.sock
-            self._clients.append(request.sock)
-
-            response.stream = True
-            response.close = False
-
-            return response
-
     @handler("request", filter=True, priority=0.2)
     def _on_request(self, request, response):
         if self.path is not None and not request.path.startswith(self.path):
             return
 
-        return self.push(WebSocketRequest(request, response))
+        headers = request.headers
 
-(Server(("0.0.0.0", 8000))
+        key1 = headers["Sec-WebSocket-Key1"]
+        key2 = headers["Sec-WebSocket-Key2"]
+
+        # Count spaces
+        nums1 = key1.count(" ")
+        nums2 = key2.count(" ")
+
+        # Join digits in the key
+        num1 = ''.join([x for x in key1 if x.isdigit()])
+        num2 = ''.join([x for x in key2 if x.isdigit()])
+
+        # Divide the digits by the num of spaces
+        key1 = int(int(num1) / int(nums1))
+        key2 = int(int(num2) / int(nums2))
+
+        # Pack into Network byte ordered 32 bit ints
+        key1 = pack("!I", key1)
+        key2 = pack("!I", key2)
+
+        # Concat key1, key2, and the the body of the client handshake
+        # and take the md5 sum of it
+        key = key1 + key2 + request.body.read()
+        m = md5()
+        m.update(key)
+        d = m.digest()
+
+        del response.headers["Content-Type"]
+
+        response.code = 101
+        response.message = "WebSocket Protocol Handshake"
+        response.headers["Upgrade"] = "WebSocket"
+        response.headers["Connection"] = "Upgrade"
+        response.headers["Sec-WebSocket-Origin"] = "http://127.0.0.1:8888"
+        response.headers["Sec-WebSocket-Location"] = \
+                "ws://localhost:8888/websocket"
+        response.headers["Sec-WebSocket-Protocol"] = "chat"
+
+        self._clients.append(request.sock)
+
+        response.close = False
+
+        return d
+
+(Server(("127.0.0.1", 8888))
     + WebSockets("/websocket")
     + Debugger()
     + Static()
