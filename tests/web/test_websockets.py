2526014fbe3668edb45f0eae9712d5efd17c81b9
prologic <none@none>
2011-02-28 23:18:41 +1000

    - tests.web: ``test_websockets`` re-enabled as it now passes again   (hopefully consistently thanks to the websocket.py client module [1])
    [1] websocket.py form http://pypi.python.org/pypi/websocket-client/

--- tests/web/test_websockets.py
+++ tests/web/test_websockets.py
@@ -1,40 +1,15 @@
 #!/usr/bin/env python
 
-import pytest
+from urlparse import urlunsplit
 
-pytest.skip("Unreliable test. Rewrite!")
-
-from random import choice, randint
-
-from circuits import handler, Component
-from circuits.net.sockets import Close, Write
+from circuits import Component
+from circuits.net.sockets import Write
 from circuits.web.dispatchers import WebSockets
-from circuits.web.client import Client, Connect, Request
+
+from websocket import create_connection
 
 
-def generate_key():
-    spaces = randint(1, 12)
-    maxnum = 4294967295 / spaces
-    number = randint(0, maxnum)
-    product = number * spaces
-    key = str(product)
-    available_chars = range(0x21, 0x2f + 1) + range(0x3a, 0x7e + 1)
-    n = randint(1, 12)
-    for _ in xrange(n):
-        ch = choice(available_chars)
-        pos = randint(0, len(key))
-        key = key[0:pos] + chr(ch) + key[pos:]
-    for _ in xrange(spaces):
-        pos = randint(1, len(key) - 1)
-        key = key[0:pos] + " " + key[pos:]
-    return number, key
-
-
-def generate_key3():
-    return "".join([chr(randint(0, 255)) for _ in xrange(8)])
-
-
-class Echo(Component):
+class Test1(Component):
 
     channel = "ws"
 
@@ -42,105 +17,26 @@ class Echo(Component):
         self.push(Write(sock, data))
 
 
-class WebSocketsClient(Component):
+class Test2(Component):
 
-    channel = "web"
+    channel = "ws"
 
-    def __init__(self, url):
-        super(WebSocketsClient, self).__init__()
-
-        self._buffer = ""
-        self._handshake = False
-        self._messages = []
-
-        self._client = Client(url).register(self)
-
-    def _parse_messages(self):
-        msgs = []
-        end_idx = 0
-        buf = self._buffer
-        while buf:
-            frame_type = ord(buf[0])
-            if frame_type == 0:
-                # Normal message.
-                end_idx = buf.find("\xFF")
-                if end_idx == -1:  # pragma NO COVER
-                    break
-                msgs.append(buf[1:end_idx].decode("utf-8", "replace"))
-                buf = buf[(end_idx + 1):]
-            elif frame_type == 255:
-                # Closing handshake.
-                assert ord(buf[1]) == 0, \
-                        "Unexpected closing handshake: %r" % buf
-                self.closed = True
-                break
-            else:
-                raise ValueError(
-                    "Don't understand how to parse "
-                    "this type of message: %r" % buf)
-        self._buffer = buf
-        return msgs
-
-    @property
-    def connected(self):
-        if hasattr(self, "_client"):
-            return self._client.connected
-
-    @handler("read", filter=True, target="client")
-    def _on_client_read(self, data):
-        if self._handshake:
-            self._buffer += data
-            self._messages.extend(self._parse_messages())
-            return True
-
-    @handler("response")
-    def _on_response(self, response):
-        if (response.status == 101
-                and response.message == "WebSocket Protocol Handshake"):
-            self._handshake = True
+    def message(self, sock, data):
+        return data
 
 
-def test(webapp):
-    Echo().register(webapp)
+def test1(webapp):
+    Test1().register(webapp)
     WebSockets("/websocket").register(webapp)
 
-    client = WebSocketsClient(webapp.server.base)
-    client.start()
+    host = webapp.server.host
+    if webapp.server.port is not 80:
+        host = "%s:%d" % (host, webapp.server.port)
 
-    client.push(Connect())
-    assert pytest.wait_for(client, "connected")
+    url = urlunsplit(("ws", host, "/websocket", "", ""))
 
-    number1, key1 = generate_key()
-    number2, key2 = generate_key()
-    body = generate_key3()
-
-    method = "GET"
-    url = "/websocket"
-    headers = {
-            "Upgrade": "WebSocket",
-            "Connection": "Upgrade",
-            "Host": "127.0.0.1:8000",
-            "Origin": "http://127.0.0.1:8000",
-            "Sec-WebSocket-Key1": key1,
-            "Sec-WebSocket-Key2": key2,
-    }
-
-    client.push(Request(method, url, body, headers))
-    assert pytest.wait_for(client, "_handshake")
-
-    response = client._client.response
-    assert response.status == 101
-    assert response.message == "WebSocket Protocol Handshake"
-
-    client.push(Write("\x00Hello World!\xff"))
-
-    def test(obj, attr):
-        messages = getattr(obj, attr)
-        return messages and messages[0] == "Hello World!"
-
-    assert pytest.wait_for(client, "_messages", test)
-
-    client.push(Close())
-    assert pytest.wait_for(client, "connected", False)
-
-    client.stop()
+    ws = create_connection(url)
+    ws.send("Hello World!")
+    result =  ws.recv()
+    assert result == "Hello World!"
+    ws.close()
