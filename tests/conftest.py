7b0845bb7b15fc5bd48706f3427bab389791a101
jamesmills <none@none>
2011-03-22 07:04:56 +0000

    - tests: Added ``conftests.wait_event`` to support tests that need to wait for   a specific event during testing.

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,6 +9,26 @@ from time import sleep
 import collections
 
 
+def wait_event(m, channel, target=None, timeout=30.0):
+    from circuits.core.manager import TIMEOUT
+
+    flag = False
+
+    def handler(evt, handler, retval):
+        nonlocal flag
+        flag = True
+
+    if target is None:
+        target = m
+
+    m.addHandler(handler, channel, target=target)
+
+    for i in range(int(timeout / TIMEOUT)):
+        if flag:
+            return True
+        sleep(TIMEOUT)
+
+
 def wait_for(obj, attr, value=True, timeout=30.0):
     from circuits.core.manager import TIMEOUT
     for i in range(int(timeout / TIMEOUT)):
@@ -18,10 +38,10 @@ def wait_for(obj, attr, value=True, timeout=30.0):
         elif getattr(obj, attr) == value:
             return True
         sleep(TIMEOUT)
-    return False
 
 
 def pytest_namespace():
     return dict((
+        ("wait_event", wait_event),
         ("wait_for", wait_for),
     ))
