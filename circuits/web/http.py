376cfca3e9f0655442e4f14e82b81d4a27472a5f
jamesmills <none@none>
2011-04-27 23:19:06 +0000

    Use self._encoding

--- circuits/web/http.py
+++ circuits/web/http.py
@@ -74,7 +74,9 @@ class HTTP(BaseComponent):
 
     @handler("response")
     def _on_response(self, response):
-        self.push(Write(response.request.sock, str(response).encode(HTTP_ENCODING)))
+        self.push(
+                Write(response.request.sock,
+                    str(response).encode(self._encoding)))
 
         if response.stream and response.body:
             try:
@@ -83,7 +85,8 @@ class HTTP(BaseComponent):
                 data = None
             self.push(Stream(response, data))
         else:
-            parts = (s if isinstance(s, bytes) else s.encode(self._encoding) for s in response.body)
+            parts = (s if isinstance(s, bytes) else s.encode(self._encoding) \
+                    for s in response.body)
             body = b"".join(parts)
 
             if body:
