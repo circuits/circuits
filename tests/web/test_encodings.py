aa274cbdd3998f8d449ebc4682ebe3b938d077ee
Osso <adeiana@gmail.com>
2011-04-28 01:44:25 +0200

    fixes test for py3

--- tests/web/test_encodings.py
+++ tests/web/test_encodings.py
@@ -8,10 +8,10 @@ class Root(Controller):
         return "Hello World!".encode('utf-16be')
 
     def foo(self):
-        return b"Hello World!".encode('utf-16be')
+        return b"Hello World!".decode()
 
 
-def test_utf_16(webapp):
+def test_utf_16_manual(webapp):
     f = urlopen(webapp.server.base)
     s = f.read()
     assert s == "Hello World!".encode('utf-16be')
@@ -19,14 +19,13 @@ def test_utf_16(webapp):
 
 def test_utf_16_server():
     from circuits import Component
-    from circuits import Debugger
 
     class WebApp(Component):
         def __init__(self):
             super(WebApp, self).__init__()
             from circuits.web import Server
             self.server = Server(0, encoding='utf-16be').register(self)
-    
+
     webapp = WebApp()
     Root().register(webapp)
 
