a192a305ba1dfc6eb5f240ae1987268f9b13a150
Osso <adeiana@gmail.com>
2011-04-27 23:28:09 +0200

    added tests for http using an encoding that is not utf-8

---
+++ tests/web/test_encodings.py
@@ -0,0 +1,40 @@
+from circuits.web import Controller
+
+from .helpers import urlencode, urlopen, HTTPError
+
+class Root(Controller):
+
+    def index(self):
+        return "Hello World!".encode('utf-16be')
+
+    def foo(self):
+        return b"Hello World!".encode('utf-16be')
+
+
+def test_utf_16(webapp):
+    f = urlopen(webapp.server.base)
+    s = f.read()
+    assert s == "Hello World!".encode('utf-16be')
+
+
+def test_utf_16_server():
+    from circuits import Component
+    from circuits import Debugger
+
+    class WebApp(Component):
+        def __init__(self):
+            super(WebApp, self).__init__()
+            from circuits.web import Server
+            self.server = Server(0, encoding='utf-16be').register(self)
+    
+    webapp = WebApp()
+    Root().register(webapp)
+
+    webapp.start()
+
+    try:
+        f = urlopen("%s/foo" % webapp.server.base)
+        s = f.read()
+        assert s == "Hello World!".encode('utf-16be')
+    finally:
+        webapp.stop()
