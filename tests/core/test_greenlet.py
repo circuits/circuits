88dad7b35f204054a1215fcbbc5637798bad2539
Osso <adeiana@gmail.com>
2011-04-30 08:20:44 +0200

    fixed greenlet test

--- tests/core/test_greenlet.py
+++ tests/core/test_greenlet.py
@@ -20,15 +20,10 @@ class BarDone(Event):
 class Test(Component):
 
     def test_wait_class(self):
-        x = self.fire(Bar())
-        self.waitEvent(Bar)
-        return x.value
+        self.fire(Bar())
 
     def test_wait_instance(self):
-        e = Bar()
-        x = self.fire(e)
-        self.waitEvent(e)
-        return x.value
+        self.fire(Bar())
 
     def bar(self):
         return "Foobar!"
@@ -53,9 +48,10 @@ def test_wait_instance():
     test = Test()
     test.run()
 
-    x = test.fire(Foo(), "test_wait_instance")
+    e = Foo()
+    x = test.fire(e, "test_wait_instance")
 
-    x = test.waitEvent(Bar, 30)
+    x = test.waitEvent(e, 30)
 
     value = x.value.value
     assert value == "Foobar!"
