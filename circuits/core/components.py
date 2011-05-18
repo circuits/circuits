5c39d5dacddeafae89a9e12a58ff38228370a428
Osso <adeiana@gmail.com>
2011-05-19 00:59:29 +0200

    first fix for method handler

--- circuits/core/components.py
+++ circuits/core/components.py
@@ -78,7 +78,8 @@ class BaseComponent(Manager):
 
     def _registerHandlers(self, manager=None):
         if manager is None:
-            p = lambda x: isinstance(x, collections.Callable) and getattr(x, "handler", False)
+            p = lambda x: isinstance(x, collections.Callable) \
+                and getattr(x, "handler", False) is True
             handlers = [v for k, v in getmembers(self, p)]
             for handler in handlers:
                 target = handler.target or getattr(self, "channel", "*")
