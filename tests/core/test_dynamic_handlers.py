4f975f137b8bee3f081e4647fac80e5e1e894206
prologic <none@none>
2011-05-27 08:33:36 +1000

    Added removeHandler test case

--- tests/core/test_dynamic_handlers.py
+++ tests/core/test_dynamic_handlers.py
@@ -25,3 +25,30 @@ def test_addHandler():
     assert s == "Hello World!"
 
     m.stop()
+
+
+def test_removeHandler():
+    m = Manager()
+    m.start()
+
+    m.addHandler(on_foo)
+
+    x = m.fire(Foo())
+
+    pytest.wait_event(m, "foo")
+
+    s = x.value[0]
+    assert s == "Hello World!"
+
+    m.removeHandler(on_foo)
+
+    x = m.fire(Foo())
+
+    pytest.wait_event(m, "foo")
+
+    assert x.value is None
+
+    assert on_foo not in dir(m)
+    assert len(m._handlers["foo"]) == 0
+
+    m.stop()
