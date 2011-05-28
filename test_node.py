8e35af81279bc2e711456bd09a358bf4a53dbc32
prologic <none@none>
2011-05-28 10:24:46 +1000

    Updated manual test for node with better debugging output

--- test_node.py
+++ test_node.py
@@ -13,22 +13,15 @@ class App(Component):
     def foo(self):
         return "Hello World!"
 
-a1 = App() + Debugger()
+a1 = App() + Debugger(trim=70, prefix="a1")
 n1 = Node().register(a1)
 a1.start()
 
-a2 = App() + Debugger()
+a2 = App() + Debugger(trim=70, prefix="a2")
 n2 = Node(("127.0.0.1", 7000)).register(a2)
 a2.start(process=True)
 
-n1.add('bar', "127.0.0.1", 7000)
+n1.add("test", "127.0.0.1", 7000)
 
-e = Event.create('foo')
-e.notify = True
-r = Remote(e, 'bar')
-r.notify = True
-a1.fire(r)
-
-
-import time
-time.sleep(3000)
+e = Event.create("foo")
+r = Remote(e, "test")
