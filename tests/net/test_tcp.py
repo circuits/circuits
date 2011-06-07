b97cccfe1234de9f0ca957a25dd3e50931edf52e
Osso <adeiana@gmail.com>
2011-06-07 22:41:43 +0200

    fixes randomly failing tcp tests

--- tests/net/test_tcp.py
+++ tests/net/test_tcp.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
 
 from time import time
-from random import seed, randint
-
+from socket import socket, AF_INET, SOCK_STREAM
 import pytest
 
 from circuits import Manager
@@ -12,6 +11,11 @@ from circuits.net.sockets import Close, Connect, Write
 
 from .client import Client
 from .server import Server
+
+def wait_host(server):
+    def checker(obj, attr):
+        return all((getattr(obj, a) for a in attr))
+    assert pytest.wait_for(server, ("host", "port"), checker)
 
 
 def pytest_generate_tests(metafunc):
@@ -53,6 +57,7 @@ def test_tcp_basic(Poller):
     try:
         assert pytest.wait_for(client, "ready")
         assert pytest.wait_for(server, "ready")
+        wait_host(server)
 
         client.fire(Connect(server.host, server.port))
         assert pytest.wait_for(client, "connected")
@@ -86,6 +91,7 @@ def test_tcp_reconnect(Poller):
     try:
         assert pytest.wait_for(client, "ready")
         assert pytest.wait_for(server, "ready")
+        wait_host(server)
 
         # 1st connect
         client.fire(Connect(server.host, server.port))
@@ -133,6 +139,7 @@ def test_tcp_connect_closed_port(Poller):
     try:
         assert pytest.wait_for(client, "ready")
         assert pytest.wait_for(server, "ready")
+        wait_host(server)
 
         host, port = server.host, server.port
         tcp_server._sock.close()
@@ -150,8 +157,10 @@ def test_tcp_connect_closed_port(Poller):
 def test_tcp_bind(Poller):
     m = Manager() + Poller()
 
-    seed(time())
-    bind_port = randint(1000, 65535)
+    sock = socket(AF_INET, SOCK_STREAM)
+    sock.listen(0)
+    _, bind_port = sock.getsockname()
+    sock.close()
 
     server = Server() + TCPServer(0)
     client = Client() + TCPClient(bind_port)
@@ -164,6 +173,7 @@ def test_tcp_bind(Poller):
     try:
         assert pytest.wait_for(client, "ready")
         assert pytest.wait_for(server, "ready")
+        wait_host(server)
 
         client.fire(Connect(server.host, server.port))
         assert pytest.wait_for(client, "connected")
