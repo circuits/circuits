bb1e202f659f250c664116f433fd9306a844157a
Alessio Deiana <adeiana@gmail.com>
2011-06-14 15:28:18 +0200

    make coverage optional

--- tests/core/signalapp.py
+++ tests/core/signalapp.py
@@ -3,7 +3,11 @@
 import os
 import sys
 
-from coverage import coverage
+try:
+    from coverage import coverage
+    HAS_COVERAGE = True
+except ImportError:
+    HAS_COVERAGE = False
 
 from circuits import Component
 from circuits.app import Daemon
@@ -24,15 +28,17 @@ class App(Component):
         f.close()
 
 def main():
-    _coverage = coverage(data_suffix=True)
-    _coverage.start()
+    if HAS_COVERAGE:
+        _coverage = coverage(data_suffix=True)
+        _coverage.start()
 
     pidfile = os.path.abspath(sys.argv[1])
     signalfile = os.path.abspath(sys.argv[2])
     App(pidfile, signalfile).run()
 
-    _coverage.stop()
-    _coverage.save()
+    if HAS_COVERAGE:
+        _coverage.stop()
+        _coverage.save()
 
 if __name__ == "__main__":
     main()
