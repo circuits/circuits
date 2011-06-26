be1b98d4aadbcc6f51c30538efda7f17f9317b51
prologic <none@none>
2011-06-26 14:20:04 +1000

    Set devel = False in docs for stable branch

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -54,7 +54,7 @@ release = '1.6'
 
 # Devel or Release mode for the documentation (if devel, include TODOs,
 # can also be used in conditionals: .. ifconfig :: devel)
-devel = True
+devel = False
 
 if devel:
     release += "dev"
