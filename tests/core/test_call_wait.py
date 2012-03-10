dbc8e3df8e3ae9483290d01f70dcb6c1e9309c03
Alessio Deiana <adeiana@gmail.com>
2012-03-10 13:14:08 +0100

    adds 2 failing tests for yield

--- tests/core/test_call_wait.py
+++ tests/core/test_call_wait.py
@@ -13,10 +13,22 @@ class TestCall(Event):
     """TestCall Event"""
 
 
+class TestLongCall(Event):
+    """TestLongCall Event"""
+
+
+class TestLongWait(Event):
+    """TestLongCall Event"""
+
+
 class Hello(Event):
     """Hello Event"""
 
     success = True
+
+
+class Foo(Event):
+    """Foo Event"""
 
 
 class Test(Component):
@@ -31,6 +43,18 @@ class Test(Component):
 
     def hello(self):
         return "Hello World!"
+
+    def test_long_wait(self):
+        x = self.fire(Foo())
+        yield self.wait("foo")
+        yield x.value
+
+    def test_long_call(self):
+        yield self.call(Foo())
+
+    def foo(self):
+        for i in xrange(1, 10):
+            yield i
 
 
 def test_wait():
@@ -57,5 +81,37 @@ def test_call():
 
     value = x.value
     assert value == "Hello World!"
+
+    test.stop()
+
+
+def test_long_call():
+    test = Test()
+    from circuits import Debugger
+    Debugger().register(test)
+    test.start()
+
+    waiter = pytest.WaitEvent(test, "test_long_call")
+    x = test.fire(TestLongCall())
+    waiter.wait()
+
+    value = x.value
+    assert value == range(1, 10)
+
+    test.stop()
+
+
+def test_long_wait():
+    test = Test()
+    from circuits import Debugger
+    Debugger().register(test)
+    test.start()
+
+    waiter = pytest.WaitEvent(test, "test_long_wait")
+    x = test.fire(TestLongWait())
+    waiter.wait()
+
+    value = x.value
+    assert value == range(1, 10)
 
     test.stop()
