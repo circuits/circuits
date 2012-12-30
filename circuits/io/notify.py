c50be8cb5b52e568235b948a629b75757f6cfac0
prologic <none@none>
2012-12-30 14:37:07 +1000

    Reworked Notifier Component to be more async friendly

--- circuits/io/notify.py
+++ circuits/io/notify.py
@@ -16,9 +16,15 @@ try:
 except ImportError:
     raise Exception("No pyinotify support available. Is pyinotify installed?")
 
+from circuits.core.utils import findcmp
 from circuits.core import handler, Component, Event
+from circuits.core.pollers import BasePoller, Poller
 
 MASK = ALL_EVENTS
+
+
+class Ready(Event):
+    """Ready Event"""
 
 
 class AddPath(Event):
@@ -84,17 +90,12 @@ class Notify(Component):
 
     channel = "notify"
 
-    def __init__(self, timeout=0.01, channel=channel):
+    def __init__(self, channel=channel):
         super(Notify, self).__init__(channel=channel)
 
+        self._poller = None
         self._wm = WatchManager()
-        self._notifier = Notifier(self._wm, self._process, timeout=timeout)
-
-    @handler("generate_events", priority=-9, filter=True)
-    def _on_generate_events(self, event):
-        self._notifier.process_events()
-        if self._notifier.check_events():
-            self._notifier.read_events()
+        self._notifier = Notifier(self._wm, self._process)
 
     def _process(self, event):
         dir = event.dir
@@ -115,3 +116,37 @@ class Notify(Component):
         wd = self._wm.get_wd(path)
         if wd:
             self._wm.rm_watch(wd, rec=recursive)
+
+    @handler("ready")
+    def _on_ready(self, component):
+        self._poller.addReader(self, self._notifier._fd)
+
+    @handler("registered", channel="*")
+    def _on_registered(self, component, manager):
+        if self._poller is None:
+            if isinstance(component, BasePoller):
+                self._poller = component
+                self.fire(Ready(self))
+            else:
+                if component is not self:
+                    return
+                component = findcmp(self.root, BasePoller)
+                if component is not None:
+                    self._poller = component
+                    self.fire(Ready(self))
+                else:
+                    self._poller = Poller().register(self)
+                    self.fire(Ready(self))
+
+    @handler("started", filter=True, channel="*")
+    def _on_started(self, component):
+        if self._poller is None:
+            self._poller = Poller().register(self)
+            self.fire(Ready(self))
+
+            return True
+
+    @handler("_read", filter=True)
+    def __on_read(self, fd):
+        self._notifier.read_events()
+        self._notifier.process_events()
