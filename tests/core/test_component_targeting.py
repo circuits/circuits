9bfa9281d60fe1875260ab529ed756982397315b
prologic <none@none>
2012-12-31 17:16:17 +1000

    Added a failing tests for [#41644025]

---
+++ tests/core/test_component_targeting.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+
+import pytest
+
+from circuits import Component, Event
+
+
+class App(Component):
+
+    channel = "app"
+
+    def hello(self):
+        return "Hello World!"
+
+    def registered(self, component, manager):
+        if component is self:
+            self.fire(Event.create("Ready"))
+
+
+@pytest.fixture(scope="module")
+def app(request, manager, watcher):
+    app = App().register(manager)
+
+    def finalizer():
+        app.unregister()
+
+    request.addfinalizer(finalizer)
+
+    assert watcher.wait("ready")
+
+    return app
+
+
+def test(manager, watcher, app):
+    x = manager.fire(Event.create("Hello"), app)
+    assert watcher.wait("hello")
+    assert x == "Hello World!"
