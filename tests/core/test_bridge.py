abf83c6d9fc578879efd8c2c7ed64b60f0a0f953
prologic <none@none>
2013-01-07 22:00:16 +1000

    Removed superfluous bridge test

--- tests/core/test_bridge.py
+++ tests/core/test_bridge.py
@@ -18,34 +18,7 @@ class App(Component):
         return "Hello from {0:d}".format(getpid())
 
 
-def test_parent_child():
-    app = App()
-    waiter = pytest.WaitEvent(app, "ready")
-    app.start(process=True, link=True)
-    assert waiter.wait()
-
-    x = app.fire(Hello())
-
-    def test(obj, attr):
-        return isinstance(obj._value, list)
-
-    assert pytest.wait_for(x, None, test)
-
-    expected_set = set(
-        [
-            "Hello from {0:d}".format(getpid()),
-            "Hello from {0:d}".format(app._process.pid)
-        ]
-    )
-
-    actual_set = set(x.value)
-
-    assert actual_set == expected_set
-
-    app.stop()
-
-
-def test_child(manager, watcher):
+def test(manager, watcher):
     app = App()
     app.start(process=True, link=manager)
     assert watcher.wait("ready")
