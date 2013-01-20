b4104fa6bef14c557c5cac9c07ecdac7551d864f
prologic <none@none>
2013-01-20 16:22:03 +1000

    Fixed SSL support for circuits.web [Delivers #42007509]

--- examples/web/sslserver.py
+++ examples/web/sslserver.py
@@ -8,7 +8,5 @@ class Root(Controller):
     def index(self):
         return "Hello World!"
 
-(
-    Server(("localhost", 8000), ssl=True, certfile="cert.pem")
-    + Root()
-).run()
+
+(Server(("localhost", 8000), secure=True, certfile="cert.pem") + Root()).run()
