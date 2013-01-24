b1ae8fa743d2e078e1ead47d1f3a593e332efabb
prologic <none@none>
2013-01-25 02:06:12 +1000

    Fixed a TypeError being raised in the request handler for websockets dispatcher

--- circuits/web/dispatchers/websockets.py
+++ circuits/web/dispatchers/websockets.py
@@ -57,7 +57,7 @@ class WebSockets(BaseComponent):
 
         self._protocol_version = 13 
         headers = request.headers
-        sec_key = headers.get("Sec-WebSocket-Key", None)
+        sec_key = headers.get("Sec-WebSocket-Key", "").encode("utf-8")
         connection_tokens = [s.strip() for s in \
                              headers.get("Connection", "").lower().split(",")]
         
