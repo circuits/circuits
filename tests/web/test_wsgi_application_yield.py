90fc92fb61d91d1a64430070a252e27ccf392b6e
prologic <none@none>
2013-01-26 02:24:48 +1000

    Code cleanup

--- tests/web/test_wsgi_application_yield.py
+++ tests/web/test_wsgi_application_yield.py
@@ -1,10 +1,5 @@
 #!/usr/bin/env python
 
-try:
-    from urllib.request import urlopen
-except ImportError:
-    from urllib2 import urlopen
-
 from circuits.web import Controller
 from circuits.web.wsgi import Application
 
@@ -18,6 +13,7 @@ class Root(Controller):
         yield "World!"
 
 application = Application() + Root()
+
 
 def test(webapp):
     f = urlopen(webapp.server.base)
