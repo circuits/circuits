7888f0893bc6417e4b8009067e489085318fc731
mnlipp <none@none>
2013-02-03 21:34:03 +0100

    Reverted to old timer implementation. Cannot find bug in new version that only occurs in CI/py32 environment (cannot be reproduced locally).

--- circuits/core/timers.py
+++ circuits/core/timers.py
@@ -11,40 +11,6 @@ from time import time, mktime
 from datetime import datetime
 
 from .components import BaseComponent
-from circuits.core.utils import findcmp
-from threading import RLock
-
-
-class TimerSchedule(BaseComponent):
-    
-    singleton = True
-    _timers = []
-    
-    def add_timer(self, timer):
-        i = 0;
-        while i < len(self._timers) and timer.expiry > self._timers[i].expiry:
-            i += 1
-        self._timers.insert(i, timer)
-    
-    def remove_timer(self, timer):
-        self._timers.remove(timer)
-
-    @handler("generate_events")
-    def _on_generate_events(self, event):
-        now = time()
-        expired = [timer for timer in self._timers if timer.expiry <= now]
-        if expired:
-            for timer in expired:
-                timer.trigger()
-            event.reduce_time_left(0)
-        next_at = self.next_expiry()
-        if next_at is not None:
-            event.reduce_time_left(next_at - now)
-
-    def next_expiry(self):
-        if len(self._timers) == 0:
-            return None
-        return self._timers[0].expiry
 
 
 class Timer(BaseComponent):
@@ -69,8 +35,6 @@ class Timer(BaseComponent):
         event once and then unregisters itself.
         """
         super(Timer, self).__init__()
-        self._schedule = None
-        self._lock = RLock()
 
         if isinstance(interval, datetime):
             self.interval = mktime(interval.timetuple()) - time()
@@ -82,48 +46,31 @@ class Timer(BaseComponent):
 
         self.persist = kwargs.get("persist", False)
 
-        self._eTime = time() + self.interval
+        self.reset()
 
-    @handler("registered", channel="*")
-    def _on_registered(self, component, manager):
-        if self._schedule is None:
-            if isinstance(component, TimerSchedule):
-                self._schedule = component
-            elif component == self:
-                component = findcmp(self.root, TimerSchedule)
-                if component is not None:
-                    self._schedule = component
-                else:
-                    self._schedule = TimerSchedule().register(self)
-            if self._schedule is not None:
-                self._schedule.add_timer(self)
+    @handler("generate_events")
+    def _on_generate_events(self, event):
+        now = time()
+        if now >= self._eTime:
+            if self.unregister_pending:
+                return
+            self.fire(self.event, *self.channels)
 
-    def unregister(self):
-        with self._lock: # Concurrent reset may interfere
-            if self._schedule is not None:
-                self._schedule.remove_timer(self)
-                self._schedule = None
-        super(Timer, self).unregister()
-
-    def trigger(self):
-        self.fire(self.event, *self.channels)
-
-        if self.persist:
-            self.reset()
+            if self.persist:
+                self.reset()
+            else:
+                self.unregister()
+            event.reduce_time_left(0)
         else:
-            self.unregister()
+            event.reduce_time_left(self._eTime - now)
 
     def reset(self):
         """
         Reset the timer, i.e. clear the amount of time already waited
         for.
         """
-        with self._lock: # Concurrent unregister may interfere
-            if self._schedule is not None:
-                self._schedule.remove_timer(self)
-            self._eTime = time() + self.interval
-            if self._schedule is not None:
-                self._schedule.add_timer(self)
+
+        self._eTime = time() + self.interval
 
     @property
     def expiry(self):
