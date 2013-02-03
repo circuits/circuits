ae73fbaad285d364dabeef08d0dc9812d27c4c2d
mnlipp <none@none>
2013-02-03 13:46:48 +0100

    Hopefully finally fixed problems with timer test on CI platform.

--- tests/core/test_timers.py
+++ tests/core/test_timers.py
@@ -1,6 +1,7 @@
 # Module:   test_timers
 # Date:     10th February 2010
 # Author:   James Mills, prologic at shortcircuit dot net dot au
+import time
 
 """Timers Tests"""
 
@@ -35,12 +36,20 @@ class Test(Event):
 
 class App(Component):
 
-    flag = False
+    def __init__(self):
+        super(App, self).__init__()
+        self.flag = False
+        self.count = 0
+        self.timestamps = []
 
     def reset(self):
+        self.timestamps = []
         self.flag = False
+        self.count = 0
 
     def test(self):
+        self.timestamps.append(time.time())
+        self.count += 1
         self.flag = True
 
 
@@ -52,12 +61,17 @@ def test_timer(app):
 
 
 def test_persistentTimer(app):
-    timer = Timer(0.2, Test(), "timer", persist=True)
+    app.timestamps.append(time.time())
+    timer = Timer(0.1, Test(), "timer", persist=True)
     timer.register(app)
 
-    for i in range(2):
-        assert pytest.wait_for(app, "flag")
-        app.reset()
+    assert pytest.wait_for(app, "count", 2)
+    delta = app.timestamps[1] - app.timestamps[0]
+    # Should be 0.1, but varies depending on timer precision and load
+    assert delta >= 0.08 and delta < 0.5
+    delta = app.timestamps[2] - app.timestamps[1]
+    assert delta >= 0.08 and delta < 0.5
+    app.reset()
 
     timer.unregister()
 
