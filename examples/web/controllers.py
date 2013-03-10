e8036faf121cf88f79cb7da1083eb8c5d9a7474f
James Mills <prologic@shortcircuit.net.au>
2013-03-10 15:14:25 +1000

    Ooops - Re-fixed example to what is was :)

--- examples/web/controllers.py
+++ examples/web/controllers.py
@@ -1,15 +1,11 @@
 #!/usr/bin/env python
 
-from os import path
-
 from circuits.web import Server, Controller, Logger
 
 
 class Root(Controller):
 
     def index(self):
-        from circuits.tools import graph
-        graph(self.root)
-        return self.serve_file(path.abspath("Server.png"))
+        return "Hello World!"
 
 (Server(("0.0.0.0", 8000)) + Root() + Logger()).run()
