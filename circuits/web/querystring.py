41df2254187bd895e1884563ac0cc3a4353ced5b
spaceone <none@none>
2013-03-25 21:21:40 +0100

    use string types instead of unicode in web.querystring

--- circuits/web/querystring.py
+++ circuits/web/querystring.py
@@ -7,7 +7,7 @@ except ImportError:
     from urllib.parse import parse_qsl  # NOQA
 
 
-from circuits.six import iteritems, text_type
+from circuits.six import iteritems, string_types
 
 
 def parse(data):
@@ -27,7 +27,7 @@ class QueryStringParser(object):
     def __init__(self, data):
         self.result = {}
 
-        if isinstance(data, text_type):
+        if isinstance(data, string_types):
             sorted_pairs = self._sorted_from_string(data)
         else:
             sorted_pairs = self._sorted_from_obj(data)
