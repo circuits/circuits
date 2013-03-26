faa924bc41f53e624c492566eeb800d52f8c2839
James Mills <j.mills@griffith.edu.au>
2013-03-26 10:41:52 +1000

    Fixed ImportError due to move of websockets dispatcher

--- circuits/web/dispatchers/__init__.py
+++ circuits/web/dispatchers/__init__.py
@@ -11,13 +11,8 @@ By default a ``circuits.web.Server`` Component uses the
 
 from circuits.web.dispatchers.static import Static
 from circuits.web.dispatchers.xmlrpc import XMLRPC
-from circuits.web.dispatchers.websockets import WebSockets
+from circuits.web.dispatchers.jsonrpc import JSONRPC
 from circuits.web.dispatchers.dispatcher import Dispatcher
 from circuits.web.dispatchers.virtualhosts import VirtualHosts
-
-try:
-    from circuits.web.dispatchers.jsonrpc import JSONRPC
-except ImportError:
-    pass
 
 # flake8: noqa
