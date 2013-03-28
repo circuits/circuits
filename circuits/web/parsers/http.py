119e8bb7e19b33ba66260a8a0cbfd839ca375e04
James Mills <prologic@shortcircuit.net.au>
2013-03-28 23:41:46 +1000

    Fixed Status regex (it was wrong)

--- circuits/web/parsers/http.py
+++ circuits/web/parsers/http.py
@@ -20,7 +20,7 @@ from ..headers import Headers
 
 METHOD_RE = re.compile("^[A-Z0-9$-_.]{1,20}$")
 VERSION_RE = re.compile("^HTTP/(\d+).(\d+)$")
-STATUS_RE = re.compile("^(\d{3})(?:\s+(\w*))?$")
+STATUS_RE = re.compile("^(\d{3})(?:\s+([\s\w]*))$")
 HEADER_RE = re.compile("[\x00-\x1F\x7F()<>@,;:/\[\]={} \t\\\\\"]")
 
 # errors
