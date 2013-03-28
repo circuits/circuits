d638e6fa7b2ae67bdd9f49122749f9682f46b1b2
James Mills <prologic@shortcircuit.net.au>
2013-03-28 23:39:04 +1000

    Code cleanup

--- circuits/web/client.py
+++ circuits/web/client.py
@@ -100,11 +100,16 @@ class Client(BaseComponent):
             yield self.wait("connected", self._transport.channel)
 
         headers = Headers([(k, v) for k, v in headers.items()])
+
         # Clients MUST include Host header in HTTP/1.1 requests (RFC 2616)
         if "Host" not in headers:
-            headers["Host"] = "{0:s}:{1:s}".format(host, "" if port in (80, 443) else str(port))
+            headers["Host"] = "{0:s}:{1:s}".format(
+                host, "" if port in (80, 443) else str(port)
+            )
+
         if body is not None:
             headers["Content-Length"] = len(body)
+
         command = "%s %s HTTP/1.1" % (method, path)
         message = "%s\r\n%s" % (command, headers)
         self.fire(Write(message.encode('utf-8')), self._transport)
@@ -114,7 +119,7 @@ class Client(BaseComponent):
     @handler("response")
     def _on_response(self, response):
         self._response = response
-        if response.headers.get("Connection") == "Close":
+        if response.headers.get("Connection") == "close":
             self.fire(Close(), self._transport)
 
     @property
