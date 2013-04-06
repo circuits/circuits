46120adeaddd2bdfd19ab2e6698606f813f29c62
James Mills <prologic@shortcircuit.net.au>
2013-04-06 13:16:41 +1000

    Reviewed Filtering Demo for circuits.web [#45735111]

--- examples/web/filtering.py
+++ examples/web/filtering.py
@@ -1,19 +1,31 @@
 #!/usr/bin/env python
 
+"""Filtering
+
+A simple example showing how to intercept and potentially filter requests.
+This example demonstrates how you could intercept the response before it goes
+out changing the response's content into ALL UPPER CASE!
+"""
+
 from circuits import handler, Component
 from circuits.web import Server, Controller
 
 
 class Upper(Component):
 
-    channel = "web"
+    channel = "web"  # By default all web related events
+                     # go to the "web" channel.
 
     @handler("response", priority=1.0)
     def _on_response(self, response):
-        """Filter Response and modify it
+        """Response Handler
 
-        Filter the outgoing Response and modify it turning the
-        text of the body into uppercase.
+        Here we set the priority slightly higher than the default response
+        handler in circutis.web (0.0) so that can we do something about the
+        response before it finally goes out to the client.
+
+        Here's we're simply modifying the response body by changing the content
+        into ALL UPPERCASE!
         """
 
         response.body = "".join(response.body).upper()
@@ -22,6 +34,12 @@ class Upper(Component):
 class Root(Controller):
 
     def index(self):
+        """Request Handler
+
+        Our normal request handler simply returning
+        "Hello World!" as the response.
+        """
+
         return "Hello World!"
 
 (Server(9000) + Upper() + Root()).run()
