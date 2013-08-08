497242c038b55c55ec0bc822a20f176874b5656c
James Mills <j.mills@griffith.edu.au>
2013-08-08 13:08:14 +1000

    Document and comment hello example

--- examples/hello.py
+++ examples/hello.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+"""circuits Hello World"""
+
 from circuits import Component, Event
 
 
@@ -10,10 +12,19 @@ class Hello(Event):
 class App(Component):
 
     def hello(self):
+        """Hello Event Handler"""
+
         print("Hello World!")
 
     def started(self, component):
-        self.fire(Hello())
-        raise SystemExit(0)
+        """Started Event Handler
+
+        This is fired internally when your application starts up and can be used to
+        trigger events that only occur once during startup.
+        """
+
+        self.fire(Hello())  # Fire Hello Event
+
+        raise SystemExit(0)  # Terminate the Application
 
 App().run()
