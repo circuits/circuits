4921e7394ff4f5607ef714c592848c553af9ad83
James Mills <j.mills@griffith.edu.au>
2013-08-08 12:54:36 +1000

    Better document and commment filter example and use explicit calls

--- examples/filter.py
+++ examples/filter.py
@@ -1,5 +1,17 @@
 #!/usr/bin/env python
 
+"""Simple Event Filtering
+
+This example shows how you can filter events by using the ``Event.stop()`` method
+which prevents other event handlers listening to the event from running. When this
+example is run you should only get a single line of output "Hello World!".
+
+.. code-block:: sh
+
+    $ ./filter.py
+    Hello World!
+"""
+
 from circuits import Component, Event
 
 
@@ -10,12 +22,26 @@ class Hello(Event):
 class App(Component):
 
     def hello(self, event):
+        """Hello Event Handler"""
+
+        event.stop()  # Stop further event processing
+
         print("Hello World!")
-        event.stop()
 
     def started(self, event, component):
-        event.stop()
-        self.fire(Hello())
-        raise SystemExit(0)
+        """Started Event Handler
 
-(App() + App()).run()
+        This is fired internally when your application starts up and can be used to
+        trigger events that only occur once during startup.
+        """
+
+        event.stop()  # Stop further event processing
+        self.fire(Hello())  # Fire a Hello event
+        raise SystemExit(0)  # Terminate the application
+
+# Start and "run" the system.
+# We're deliberately creating two instnaces of ``App``
+# so we can demonstrating event filtering.
+app = App()
+App().register(app)  # 2nd App
+app.run()
