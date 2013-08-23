c54c11d188b3300d5fabd17b8d259692a8c67b12
James Mills <prologic@shortcircuit.net.au>
2013-08-23 20:04:05 +1000

    Added a full complete example of test-driven development with circuits

---
+++ examples/testing/pytest/README.rst
@@ -0,0 +1,23 @@
+Hello World Example -- Test Driven Development
+==============================================
+
+
+This is the classical "Hwllo World!" style application written in circuits using the `Test Driven Development (TDD) <http://en.wikipedia.org/wiki/Test-driven_development>`_ software development process. This example serves as a useful basis for developing your own circuits components and applications that are developed using this process. Furthermore all unit tests in the circuits framework and library of components are all written this way in an almost identical fasion.
+
+This requires the use of:
+
+- `pytest <http://pytest.org>`_
+
+To install pytest:
+    
+    $ pip install pytest
+
+
+To run this example:
+    
+    $ ./hello.py
+
+To run the unit test:
+    
+    $ py.test test_hello.py
+
---
+++ examples/testing/pytest/conftest.py
@@ -0,0 +1,116 @@
+# Module:   conftest
+# Date:     6th December 2010
+# Author:   James Mills, prologic at shortcircuit dot net dot au
+
+"""py.test config"""
+
+import pytest
+
+import sys
+import threading
+from time import sleep
+from collections import deque
+
+from circuits.core.manager import TIMEOUT
+from circuits import handler, BaseComponent, Debugger, Manager
+
+
+class Watcher(BaseComponent):
+
+    def init(self):
+        self._lock = threading.Lock()
+        self.events = deque()
+
+    @handler(channel="*", priority=999.9)
+    def _on_event(self, event, *args, **kwargs):
+        with self._lock:
+            self.events.append(event)
+
+    def wait(self, name, channel=None, timeout=6.0):
+        for i in range(int(timeout / TIMEOUT)):
+            if channel is None:
+                with self._lock:
+                    for event in self.events:
+                        if event.name == name:
+                            return True
+            else:
+                with self._lock:
+                    for event in self.events:
+                        if event.name == name and \
+                                channel in event.channels:
+                            return True
+
+            sleep(TIMEOUT)
+
+
+class Flag(object):
+    status = False
+
+
+class WaitEvent(object):
+
+    def __init__(self, manager, name, channel=None, timeout=6.0):
+        if channel is None:
+            channel = getattr(manager, "channel", None)
+
+        self.timeout = timeout
+        self.manager = manager
+
+        flag = Flag()
+
+        @handler(name, channel=channel)
+        def on_event(self, *args, **kwargs):
+            flag.status = True
+
+        self.handler = self.manager.addHandler(on_event)
+        self.flag = flag
+
+    def wait(self):
+        try:
+            for i in range(int(self.timeout / TIMEOUT)):
+                if self.flag.status:
+                    return True
+                sleep(TIMEOUT)
+        finally:
+            self.manager.removeHandler(self.handler)
+
+
+@pytest.fixture(scope="session")
+def manager(request):
+    manager = Manager()
+
+    def finalizer():
+        manager.stop()
+
+    request.addfinalizer(finalizer)
+
+    waiter = WaitEvent(manager, "started")
+    manager.start()
+    assert waiter.wait()
+
+    if request.config.option.verbose:
+        Debugger().register(manager)
+
+    return manager
+
+
+@pytest.fixture
+def watcher(request, manager):
+    watcher = Watcher().register(manager)
+
+    def finalizer():
+        waiter = WaitEvent(manager, "unregistered")
+        watcher.unregister()
+        waiter.wait()
+
+    request.addfinalizer(finalizer)
+
+    return watcher
+
+
+def pytest_namespace():
+    return dict((
+        ("WaitEvent", WaitEvent),
+        ("PLATFORM", sys.platform),
+        ("PYVER", sys.version_info[:3]),
+    ))
