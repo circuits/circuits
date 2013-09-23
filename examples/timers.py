3334724c93a206c66a078c08ab464f82eb217f94
James Mills <j.mills@griffith.edu.au>
2013-09-24 09:52:20 +1000

    Fixed timers examples

--- examples/timers.py
+++ examples/timers.py
@@ -20,9 +20,9 @@ class App(Component):
         print("Bar")
 
     def started(self, component):
-        Timer(5, Event.create("Hello")).register(self)
-        Timer(1, Event.create("Foo"), persist=True).register(self)
-        Timer(3, Event.create("Bar"), persist=True).register(self)
+        Timer(5, Event.create("hello")).register(self)
+        Timer(1, Event.create("foo"), persist=True).register(self)
+        Timer(3, Event.create("bar"), persist=True).register(self)
 
 
 App().run()
