ed3fc320e452940f918756f88c2b5ffdab8c3899
James Mills <prologic@shortcircuit.net.au>
2014-07-13 16:24:14 +1000

    Improved in-memory Sessions circuis.web Component to be easier to override as well as prevent Session hijacking

--- circuits/web/sessions.py
+++ circuits/web/sessions.py
@@ -2,41 +2,84 @@
 # Date:     22nd February 2009
 # Author:   James Mills, prologic at shortcircuit dot net dot au
 
+
 """Session Components
 
 This module implements Session Components that can be used to store
 and access persistent information.
 """
 
+
 from uuid import uuid4 as uuid
+from hashlib import sha1 as sha
 from collections import defaultdict
 
+
 from circuits import handler, Component
+
+
+def who(request):
+    """Create a SHA1 Hash of the User's IP Address and User-Agent"""
+
+    ip = request.remote.ip
+    agent = request.headers.get("User-Agent", "")
+
+    return sha("{0:s}{1:s}".format(ip, agent)).hexdigest()
+
+
+def create_session(request):
+    """Create a unique session id from the request
+
+    Returns a unique session using ``uuid4()`` and a ``sha1()`` hash
+    of the users IP Address and User Agent in the form of ``sid/who``.
+    """
+
+    return "{0:s}/{1:s}".format(uuid().hex, who(request))
+
+
+def verify_session(request, sid):
+    """Verify a User's Session
+
+    This verifies the User's Session by verifying the SHA1 Hash
+    of the User's IP Address and User-Agent match the provided
+    Session ID.
+    """
+
+    if "/" not in sid:
+        return create_session(request)
+
+    user = sid.split("/", 1)[1]
+
+    if user != who(request):
+        return create_session(request)
+
+    return sid
 
 
 class Sessions(Component):
 
     channel = "web"
 
-    def __init__(self, name="circuits.session", *args, **kwargs):
-        super(Sessions, self).__init__(*args, **kwargs)
+    def __init__(self, name="circuits.session", channel=channel):
+        super(Sessions, self).__init__(channel=channel)
 
         self._name = name
         self._data = defaultdict(dict)
 
-    def _load(self, id):
-        return self._data[id]
+    def load(self, sid):
+        return self._data[sid]
 
-    def _save(self, id, data):
-        self._data[id] = data
+    def save(self, sid, data):
+        """Save User Session Data for sid"""
 
     @handler("request", priority=10)
     def request(self, request, response):
         if self._name in request.cookie:
-            id = request.cookie[self._name].value
+            sid = request.cookie[self._name].value
+            sid = verify_session(request, sid)
         else:
-            id = str(uuid())
+            sid = create_session(request)
 
-        request.sid = id
-        request.session = self._load(id)
-        response.cookie[self._name] = id
+        request.sid = sid
+        request.session = self.load(sid)
+        response.cookie[self._name] = sid
