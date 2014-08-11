0ea154eda3bd2c7b01fef3a76f03916c88394bcd
James Mills <prologic@shortcircuit.net.au>
2014-08-11 13:24:15 +1000

    Code Cleanup

--- circuits/protocols/__init__.py
+++ circuits/protocols/__init__.py
@@ -2,12 +2,12 @@
 # Date:     13th March 2009
 # Author:   James Mills, prologic at shortcircuit dot net dot au
 
+
 """Networking Protocols
 
 This package contains components that implement various networking protocols.
 """
 
-from .irc import IRC
-from .line import Line
 
-# flake8: noqa
+from .irc import IRC  # noqa
+from .line import Line  # noqa
