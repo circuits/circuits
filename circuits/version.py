da2df5f558328e852040a5e30e06909ef5bac782
James Mills <prologic@shortcircuit.net.au>
2014-11-01 09:42:40 +1000

    Display version as 'dev' in development

--- circuits/version.py
+++ circuits/version.py
@@ -8,4 +8,8 @@ So we only have to maintain version information in one place!
 """
 
 version_info = (3, 2, 0, "dev")  # (major, minor, patch, dev?)
-version = ".".join(map(str, version_info))
+version = (
+    ".".join(map(str, version_info))
+    if version_info[-1] != "dev"
+    else "dev"
+)
