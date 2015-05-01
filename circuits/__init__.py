98fe24c2c95e57b29a8d19a125159dcf176252b0
SpaceOne <space@wechall.net>
2015-05-01 03:15:23 +0200

    add namespace package support

--- circuits/__init__.py
+++ circuits/__init__.py
@@ -25,3 +25,17 @@ from .core import handler, reprhandler, BaseComponent, Component
 from .core import Debugger, Bridge, Loader, Manager, Timer, TimeoutError
 
 # flake8: noqa
+
+# See http://peak.telecommunity.com/DevCenter/setuptools#namespace-packages
+try:
+	__import__('pkg_resources').declare_namespace(__name__)
+except ImportError:
+	from pkgutil import extend_path
+	__path__ = extend_path(__path__, __name__)
+	import os
+	for _path in __path__:
+		_path = os.path.join(_path, '__init__.py')
+		if _path != __file__ and os.path.exists(_path):
+			with open(_path) as fd:
+				exec fd in globals()
+	del os, extend_path, _path, fd
