2b4d5949cb874583fec7e51e95f693347f7e60d1
SpaceOne <space@wechall.net>
2015-05-02 02:37:31 +0200

    move is_ssl_handshake into net.utils

---
+++ circuits/net/utils.py
@@ -0,0 +1,14 @@
+"""Utilities"""
+
+def is_ssl_handshake(buf):
+    """Detect an SSLv2 or SSLv3 handshake"""
+
+    # SSLv3
+    v = buf[:3]
+    if v in ("\x16\x03\x00", "\x16\x03\x01", "\x16\x03\x02"):
+        return True
+
+    # SSLv2
+    v = list(iterbytes(buf[:2])) + [0x00, 0x00]
+    if (v[0] & 0x80 == 0x80) and ((v[0] & 0x7f) << 8 | v[1]) > 9:
+        return True
--- circuits/web/utils.py
+++ circuits/web/utils.py
@@ -27,6 +27,7 @@ except ImportError:
     from cgi import parse_qs as _parse_qs  # NOQA
 
 from circuits.six import iterbytes
+from circuits.net.utils import is_ssl_handshake  # backwards compatibility
 
 from .exceptions import RangeUnsatisfiable, RequestEntityTooLarge
 
@@ -368,17 +369,3 @@ class IOrderedDict(dict, MutableMapping):
 
     def __del__(self):
         self.clear()                # eliminate cyclical references
-
-
-def is_ssl_handshake(buf):
-    """Detect an SSLv2 or SSLv3 handshake"""
-
-    # SSLv3
-    v = buf[:3]
-    if v in ("\x16\x03\x00", "\x16\x03\x01", "\x16\x03\x02"):
-        return True
-
-    # SSLv2
-    v = list(iterbytes(buf[:2])) + [0x00, 0x00]
-    if (v[0] & 0x80 == 0x80) and ((v[0] & 0x7f) << 8 | v[1]) > 9:
-        return True
