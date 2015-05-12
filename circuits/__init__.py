9d8a1295f8ce383580986779cbb92f8282ba3d1d
SpaceOne <space@wechall.net>
2015-05-12 13:37:50 +0200

    fix python3 compatibility

--- circuits/__init__.py
+++ circuits/__init__.py
@@ -36,6 +36,7 @@ except ImportError:
 	for _path in __path__:
 		_path = os.path.join(_path, '__init__.py')
 		if _path != __file__ and os.path.exists(_path):
+			from .six import exec_
 			with open(_path) as fd:
-				exec fd in globals()
-	del os, extend_path, _path, fd
+				exec_(fd, globals())
+	del os, extend_path, _path, fd, exec_
