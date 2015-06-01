36c98f67512a88b766fb2124d5c50cb03e14abdd
James Mills <prologic@shortcircuit.net.au>
2015-06-02 00:16:05 +1000

    Add unit test for waiting for event instances (Closes #60)

---
+++ tests/core/test_call_wait_instance.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+import pytest
+
+from circuits import handler, Component, Event
+
+
+class wait(Event):
+    """wait Event"""
+
+    success = True
+
+
+class hello(Event):
+    """hello Event"""
+
+    success = True
+
+
+class App(Component):
+
+    @handler("wait")
+    def _on_wait(self):
+        e = hello()
+        x = self.fire(e)
+        yield self.wait(e)
+        yield x.value
+
+    def hello(self):
+        return "Hello World!"
+
+
+@pytest.fixture
+def app(request, manager, watcher):
+    app = App().register(manager)
+    assert watcher.wait("registered")
+
+    def finalizer():
+        app.unregister()
+
+    request.addfinalizer(finalizer)
+
+    return app
+
+
+def test_wait_instance(manager, watcher, app):
+    x = manager.fire(wait())
+    assert watcher.wait("wait_success")
+
+    value = x.value
+    assert value == "Hello World!"
