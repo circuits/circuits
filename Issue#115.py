c11106f482dba34a36eda5a23b74f82d25372a99
James Mills <prologic@shortcircuit.net.au>
2015-08-13 23:59:05 +1000

    WIP: Fixing SSL Handshake Issue (Server kind of works). See Issue #114

--- Issue#115.py
+++ Issue#115.py
@@ -3,6 +3,7 @@
 
 from __future__ import print_function
 
+import sys
 
 from circuits import Component, Debugger
 from circuits.net.sockets import TCPClient
@@ -11,11 +12,14 @@ from circuits.net.events import connect, write
 
 class App(Component):
 
-    def init(self):
+    def init(self, host=None, port="443"):
+        self.host = host or "www.google.com"
+        self.port = int(port)
+
         TCPClient().register(self)
 
     def ready(self, *args):
-        self.fire(connect("www.google.com", 443, True))
+        self.fire(connect(self.host, self.port, True))
 
     def connected(self, *args):
         self.fire(write("GET / HTTP/1.1\r\nHost: www.google.com\r\nConnection: close\r\n\r\n"))
@@ -27,5 +31,5 @@ class App(Component):
         raise SystemExit(0)
 
 
-app = App() + Debugger()
+app = App(*sys.argv[1:]) + Debugger()
 app.run()
