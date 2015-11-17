4346983989cedcfc2129ef0e33e44c95f3a95f2c
SpaceOne <space@wechall.net>
2015-11-17 19:55:24 +0100

    Also detect TLS 1.2 and 1.3 handshakes

--- circuits/net/utils.py
+++ circuits/net/utils.py
@@ -6,9 +6,9 @@ from circuits.six import iterbytes
 def is_ssl_handshake(buf):
     """Detect an SSLv2 or SSLv3 handshake"""
 
-    # SSLv3
+    # SSLv3, TLS 1.1 - 1.3
     v = buf[:3]
-    if v in ("\x16\x03\x00", "\x16\x03\x01", "\x16\x03\x02"):
+    if v in ("\x16\x03\x00", "\x16\x03\x01", "\x16\x03\x02", "\x16\x03\x03", "\x16\x03\x04"):
         return True
 
     # SSLv2
