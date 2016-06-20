b856e90bd0ca1c29b3f397115ad7d97842a76a79
James Mills <prologic@shortcircuit.net.au>
2016-06-20 01:42:29 -0700

    Fix Travis install script for OS X

--- .travis/install.sh
+++ .travis/install.sh
@@ -4,12 +4,12 @@ if [[ $TRAVIS_OS_NAME == "osx" ]]; then
   case "${TOXENV}" in
     py27)
       brew install python
-    ;;
+      ;;
     py35)
       brew install python3
-    ;;
+      ;;
     pypy)
       brew install pypy
-    ;;
+      ;;
   esac
 fi
