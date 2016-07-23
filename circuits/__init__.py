c9b9eb04dee65433f6097c7240ed59173b63db03
Adric Worley <adric.worley@epicgames.com>
2016-07-23 19:53:55 -0400

    Fix crash from deleting undefined variables (#170)
    This address a bug introduced in 3.2 that occurs when running a Circuits
    app with packaged with py2exe into a single zip file. In that case, the
    innermost if-check would never pass, the fd and exec_ variables would never be
    defined, and the final del would raise an unhandled NameError.

--- circuits/__init__.py
+++ circuits/__init__.py
@@ -31,10 +31,11 @@ except ImportError:
     from pkgutil import extend_path
     __path__ = extend_path(__path__, __name__)
     import os
+    from .six import exec_
+    fd = None
     for _path in __path__:
         _path = os.path.join(_path, '__init__.py')
         if _path != __file__ and os.path.exists(_path):
-            from .six import exec_
             with open(_path) as fd:
                 exec_(fd, globals())
     del os, extend_path, _path, fd, exec_
