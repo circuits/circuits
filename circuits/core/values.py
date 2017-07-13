15802533da5623f8ce6467ddb5298d846335dc3d
Pierre <bloodywing@users.noreply.github.com>
2017-07-13 11:14:06 +0200

    make __str__ compatible with py2
    See a similar implementation in Django:
    https://docs.djangoproject.com/en/1.11/_modules/django/utils/six/#python_2_unicode_compatible

--- circuits/core/values.py
+++ circuits/core/values.py
@@ -1,7 +1,7 @@
 """
 This defines the Value object used by components and events.
 """
-from ..six import string_types
+from ..six import string_types, PY2
 from .events import Event
 
 
@@ -72,7 +72,8 @@ class Value(object):
 
     def __str__(self):
         "x.__str__() <==> str(x)"
-
+        if PY2:
+            return self.value.encode('utf-8')
         return str(self.value)
 
     def inform(self, force=False):
