f85264299fea7f190f988fd73ff6188c1f1cfebb
Ryan Gordon <Ryan.Gordon1@ibm.com>
2019-08-06 12:33:32 +0100

    Added a test to verify BUFSIZE can now be used

---
+++ tests/net/test_server.py
@@ -0,0 +1,29 @@
+from circuits.web.servers import BaseServer
+from circuits.net.sockets import BUFSIZE
+
+
+class MockClass(BaseServer):
+    pass
+
+
+def test_dynamic_bufsize_in_baseserver():
+    bufsize = 10000
+    try:
+        # Assert when we set BUFSIZE ourselves
+        mock = MockClass(bind="0.0.0.0:1234", bufsize=bufsize)
+        # that it will be set to our given value
+        assert bufsize == mock.server._bufsize
+    finally:
+        mock.server.unregister()
+        mock.stop()
+
+
+def test_constant_bufsize_in_baseserver():
+    try:
+        # Assert when we dont set BUFSIZE ourself
+        mock = MockClass(bind="0.0.0.0:1235")
+        # that it will be set to the constant default value
+        assert mock.server._bufsize == BUFSIZE
+    finally:
+        mock.server.unregister()
+        mock.stop()
