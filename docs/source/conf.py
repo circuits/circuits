d0f4162d2ce128bba4edbe2669961899e09a9760
SpaceOne <space@wechall.net>
2023-01-03 15:43:43 +0100

    test

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -223,12 +223,12 @@ htmlhelp_basename = 'circuitsdoc'
 
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
-latex_documents = [
-    (
-        'index', 'circuits.tex', 'circuits Documentation',
-        'James Mills', 'manual'
-    ),
-]
+#latex_documents = [
+#    (
+#        'index', 'circuits.tex', 'circuits Documentation',
+#        'James Mills', 'manual'
+#    ),
+#]
 
 # The name of an image file (relative to this directory) to place at the top of
 # the title page.
