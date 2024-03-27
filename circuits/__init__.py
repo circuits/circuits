c22ca12780f4fd0f5e2a7258c5b338c63d958b26
SpaceOne <space@wechall.net>
2024-03-27 22:40:23 +0100

    fixup! ci: add dependency to setuptools for Python 3.12

--- circuits/__init__.py
+++ circuits/__init__.py
@@ -41,18 +41,14 @@ from .core import (
 try:
     __import__('pkg_resources').declare_namespace(__name__)
 except ImportError:
-    from pkgutil import extend_path
+    import importlib.metadata
 
-    __path__ = extend_path(__path__, __name__)
-    import os
-
-    fd = None
-    for _path in __path__:
-        _path = os.path.join(_path, '__init__.py')
-        if _path != __file__ and os.path.exists(_path):
-            with open(_path) as fd:
-                exec(fd.read(), globals())
-    del os, extend_path, _path, fd
+    try:
+        namespace_pkg = importlib.metadata.distribution(__name__)
+    except importlib.metadata.PackageNotFoundError:
+        pass
+    else:
+        namespace_pkg.activate()
 
 # flake8: noqa
 # pylama:skip=1
